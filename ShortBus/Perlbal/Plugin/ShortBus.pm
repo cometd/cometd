24e589cf249bfbe4a2aab81a1951e9c7c764ce9b
xantus <xantus@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2006-04-03 08:32:06 +0000

    switch to AddTimer, added an id hash
    git-svn-id: http://svn.cometd.com/trunk@30 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- ShortBus/Perlbal/Plugin/ShortBus.pm
+++ ShortBus/Perlbal/Plugin/ShortBus.pm
@@ -6,25 +6,15 @@ use warnings;
 use Data::GUID;
 
 our @listeners;
+our %ids;
 our $plugin;
 
 sub register {
     shift;
     my $service = shift;
-
-    Perlbal::Socket::register_callback( 1, sub {
-        my $count = scalar( @listeners );
-        @listeners = grep { !$_->{closed} } @listeners;
-        if ( scalar( @listeners ) != $count ) {
-            bcast_event( { connectionCount => $count } );
-        }
-        return 1;
-    } );
-    
-    Perlbal::Socket::register_callback( 5, sub {
-        bcast_event( { "time" => time() } );
-        return 5;
-    } );
+   
+    Danga::Socket->AddTimer( 1, \&connection_count );
+    Danga::Socket->AddTimer( 5, \&time_keepalive );
     
     $service->register_hook( "ShortBus", "start_proxy_request", \&start_proxy_request );
 
@@ -41,7 +31,7 @@ sub load {
 }
 
 sub unload {
-    @listeners = ();
+    @listeners = %ids = ();
     $plugin = undef;
     return 1;
 }
@@ -52,8 +42,10 @@ sub start_proxy_request {
     return 0 unless $head;
     my $uri = $head->request_uri;
     
-    return 0 unless $uri =~ m{^/shortbus/(\w+)(?:\?last=(\d+))?$};
-    my ( $action, $last ) = ( $1, $2 || 0 );
+#    return 0 unless $uri =~ m{^/shortbus/(\w+)(?:\?last=(\d+))?$};
+#    my ( $action, $last ) = ( $1, $2 || 0 );
+    my $action = 'register';
+    my $last = 0;
 
     my $send = sub {
         shift;
@@ -61,7 +53,7 @@ sub start_proxy_request {
         
         my $res = $self->{res_headers} = Perlbal::HTTPHeaders->new_response( 200 );
         $res->header( "Content-Type", "text/html" );
-        $res->header( "Content-Length", length($$body) );
+        $res->header( "Content-Length", length( $$body ) );
         $self->setup_keepalive( $res );
 
         $self->state( "xfer_resp" );
@@ -82,12 +74,15 @@ sub start_proxy_request {
         $res->header( "Connection", "close" );
         
         # TODO get guid from backend return header
-        $self->{scratch}{guid} = Data::GUID->new();
+        my $id = Data::GUID->new();
+        $self->{scratch}{guid} = $id = "$id";
+        $ids{ $id } = $self;
         push( @listeners, $self );
         
         $self->write( $res->to_string_ref );
+        $self->tcp_cork( 1 );
        
-        $self->write( \ qq|<html>
+        $self->write( qq|<html>
 <body>
 <script>
 var domain = document.domain.split('.').reverse();
@@ -109,27 +104,41 @@ if (window.parent.log) {
         
         my $last_ret = $self->write( filter(
             {
-                id => "".$self->{scratch}{guid},
+                id => $self->{scratch}{guid},
                 ( $last ? ( "last" => $last ) : () )
             }
         ) );
         
-        if ($last) {
+        if ( $last ) {
             # TODO queue
             if ( $self->{scratch}{queue} ) {
                 foreach ( @{$self->{scratch}{queue}} ) {
-                    next if ( $_->[0] < $last );
-                    $last_ret = $self->write( $_->[1] );
+                    next if ( $_->[ 0 ] < $last );
+                    $last_ret = $self->write( $_->[ 1 ] );
                 }
             }
         }
 
-        $self->watch_write(0) if ( $last_ret );
-        
+        $self->watch_write( 0 ) if ( $last_ret );
+
         return 1;
     }
 
-    if ( $action eq "bcast" ) {
+    if ( $action eq "stream" ) {
+        
+        # create filehandle for reading
+#        my $data = '';
+#        Perlbal::AIO::aio_read($read_handle, 0, 2048, $data, sub {
+            # got data? undef is error
+#            return $self->_simple_response(500) unless $_[0] > 0;
+
+#            $ids{$id}->write( filter( { event => 'stream', data => $data } ) );
+            
+            # reenable writes after we get data
+            #$self->tcp_cork(1); # by setting reproxy_file_offset above, it won't cork, so we cork it
+            #$self->watch_write(1);
+#        });
+        
         # TODO inject
         bcast_event( { data => "" } );
 
@@ -160,15 +169,32 @@ sub bcast_event {
         
     
     if ( $cleanup ) {
-        @listeners = grep { !$_->{closed} } @listeners;
+        @listeners = map { if (!$_->{closed}) { $_; } else { delete $ids{$_->{scratch}{guid}}; (); } } @listeners;
         bcast_event( { connectionCount => scalar(@listeners) } );
     }
 
 }
 
-
 sub filter {
     $plugin->freeze(shift);
+}
+
+sub connection_count {
+    Danga::Socket->AddTimer( 1, \&connection_count );
+    
+    my $count = scalar( @listeners );
+    @listeners = map { if (!$_->{closed}) { $_; } else { delete $ids{$_->{scratch}{guid}}; (); } } @listeners;
+    if ( scalar( @listeners ) != $count ) {
+        bcast_event( { connectionCount => $count } );
+    }
+    return 1;
+}
+    
+sub time_keepalive {
+    Danga::Socket->AddTimer( 5, \&time_keepalive );
+    
+    bcast_event( { "time" => time() } );
+    return 5;
 }
 
 1;
@@ -178,12 +204,12 @@ package ShortBus::Filter::JSON;
 use JSON;
 
 sub new {
-    bless({},shift);
+    bless( {},shift );
 }
 
 sub freeze {
     shift;
-    return "<script>\nsb('".objToJson(shift)."');\n</script>\n";
+    return "<script>sb('".objToJson(shift)."');</script>\n";
 }
 
 sub thaw {
