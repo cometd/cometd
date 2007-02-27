11d14ce22192057742e5f6d7dae5fb9f67b8bc04
xantus <xantus@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2007-02-27 04:52:41 +0000

    better request handling
    git-svn-id: http://svn.cometd.com/trunk@313 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-perl/lib/Cometd/Plugin/HTTP/Deny.pm
+++ cometd-perl/lib/Cometd/Plugin/HTTP/Deny.pm
@@ -41,25 +41,31 @@ sub local_connected {
 sub local_receive {
     my ( $self, $server, $con, $req ) = @_;
 
-    if ( !defined( $con->{_close} ) ) {
-        my $connection = $req->header( 'connection' );
-        $con->{_close} = 0 if ( $connection && $connection =~ m/^keep-alive$/i );
-    }
+    my $r;
     
-    $con->{_uri} ||= $req->uri;
+    if ( ref $req && $req->isa( 'HTTP::Response' ) ) {
+        $r = $con->{_r} || $req; # a prebuilt response
+        $con->{_close} = 1; # default anyway
+    } elsif ( ref $req && $req->isa( 'HTTP::Request' ) ) {
+        if ( !defined( $con->{_close} ) ) {
+            my $connection = $req->header( 'connection' );
+            $con->{_close} = 0 if ( $connection && $connection =~ m/^keep-alive$/i );
+        }
+    
+        $con->{_uri} ||= $req->uri;
 
-    $server->_log( v=> 4, msg => "403 [directory] $con->{_uri}" );
+        $server->_log( v=> 4, msg => "403 [directory] $con->{_uri}" );
     
-    my $r = $con->{_r} || HTTP::Response->new();
-    $r->header( Date => time2str( time() ) ) unless ( $r->header( 'Date' ) );
-    $r->header( Server => 'Cometd (http://cometd.com/)' );
+        $r = $con->{_r} || HTTP::Response->new();
+        $r->header( Date => time2str( time() ) ) unless ( $r->header( 'Date' ) );
+        $r->header( Server => 'Cometd (http://cometd.com/)' );
     
-    $r->code( 403 );
-    $r->content_type( 'text/html' );
-    my $out = qq|<html><head><title>403 Forbidden</title></head><body><h2>403 Forbidden</h2></body></html>\n|;
-    $r->header( 'Content-Length' => length( $out ) );
-    $r->content( $out );
-    $con->wheel->resume_input();
+        $r->code( 403 );
+        $r->content_type( 'text/html' );
+        my $out = qq|<html><head><title>403 Forbidden</title></head><body><h2>403 Forbidden</h2></body></html>\n|;
+        $r->header( 'Content-Length' => length( $out ) );
+        $r->content( $out );
+    }
     
     # XXX max requests
 #    $con->{_close} = 1 if ( $con->{__requests} && $con->{__requests} > 100 );
@@ -74,6 +80,7 @@ sub local_receive {
         $r->header( 'connection' => 'keep-alive' );
         $con->send( $r );
         $con->{__requests}++;
+        $con->wheel->resume_input();
         # release control to other plugins
         $self->release_connection( $con );
     }
