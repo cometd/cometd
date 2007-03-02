a4f044aeca838920992dbf38cf4285ccb7d23b52
xantus <xantus@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2007-03-02 22:00:31 +0000

    rename unescape to uri_unescape
    git-svn-id: http://svn.cometd.com/trunk@320 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-perl/lib/Cometd/Common.pm
+++ cometd-perl/lib/Cometd/Common.pm
@@ -4,7 +4,7 @@ our %hex_chr;
 
 BEGIN {
     for ( 0 .. 255 ) {
-        my $h = sprintf( "%02x", $_ );
+        my $h = sprintf( "%%%02X", $_ );
         $hex_chr{lc($h)} = $hex_chr{uc($h)} = chr($_);
     }
 }
@@ -15,7 +15,7 @@ sub import {
     my $package = caller();
 
     my @exports = qw(
-        unescape
+        uri_unescape
         adjust_params
     );
 
@@ -29,9 +29,9 @@ sub import {
     }
 }
 
-sub unescape {
-    my $es = shift;
-    $es =~ tr/+/ /;
+sub uri_unescape {
+    my $es = shift or return;
+    $es =~ tr/+/ /; # foo=this+is+a+test
     $es =~ s/(%[0-9a-fA-F]{2})/$hex_chr{$1}/gs;
     return $es;
 }
@@ -40,12 +40,7 @@ sub unescape {
 # my %opts = &adjust_params;
 # my $t = adjust_params($f); # $f being a hashref
 sub adjust_params {
-    my $o;
-    if ( $#_ == 0 && ref( $_[ 0 ] ) ) {
-        $o = shift;
-    } else {
-        %$o = @_;
-    }
+    my $o = ( $#_ == 0 && ref( $_[ 0 ] ) ) ? shift : { @_ };
     foreach my $k ( keys %$o ) {
         local $_ = "$k";
         s/([A-Z][a-z]+)/lc($1)."_"/ge; s/_$//;
