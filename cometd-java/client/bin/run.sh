86aa3a7070863131f5ad77d4bf51e1eeb8dcea12
Greg Wilkins <gregw@intalio.com>
2009-04-23 21:37:55 +0000

    updated run script

--- cometd-java/client/bin/run.sh
+++ cometd-java/client/bin/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cd `dirname $0`/..
-java -Xmx2048M -Djetty.home=../../../ -DSTOP.PORT=-1 -DSTART=bin/start.config -jar ../../../start.jar
+REPO=$HOME/.m2/repository
+JETTY_VERSION=7.0.0.M1
+
+java -Xmx2048M -Drepo=$REPO -DSTART=bin/start.config -jar $REPO/org/eclipse/jetty/jetty-start/$JETTY_VERSION/jetty-start-$JETTY_VERSION.jar
