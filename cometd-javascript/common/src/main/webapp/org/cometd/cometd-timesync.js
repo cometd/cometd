0f1dd3bec909a9ffcc5e62805fd3256d95fb2cf8
Simone Bordet <sbordet@intalio.com>
2009-05-10 22:46:49 +0000

    Ported comet extensions.

---
+++ cometd-javascript/common/src/main/webapp/org/cometd/cometd-timesync.js
@@ -0,0 +1,154 @@
+/**
+ * The timesync extension implements an algorithm similar to NTP (network time protocol)
+ * to derive the offset between the client time and the server time.
+ *
+ * For every message on the /meta/* channels, this extension adds the following object
+ * to the bayeux 'ext' object:
+ * <pre>
+ * timesync: {
+ *     tc: 1234567890
+ *     l:  123
+ *     o:  4567
+ * }
+ * </pre>
+ * where:
+ * <ul>
+ * <li><b>tc</b> is the client timestamp of when the message was sent, in milliseconds</li>
+ * <li><b>l</b> is the network lag that the client has calculated</li>
+ * <li><b>o</b> is the clock offset that the client has calculated</li>
+ * </ul>
+ * The accuracy of the offset and lag may be calculated with tc - now - l - o,
+ * which should be zero if the calculated offset and lag are perfectly accurate.
+ *
+ * A cometd server that supports timesync, should respond only if the
+ * measured accuracy value is greater than accuracy target.
+ * The response is an object in the bayeux 'ext' object:
+ * <pre>
+ * timesync: {
+ *     tc: 1234567890,
+ *     ts: 1234567899,
+ *     p:  123,
+ *     a:  4
+ * }
+ * </pre>
+ * where:
+ * <ul>
+ * <li>tc is the client timestamp of when the message was sent</li>
+ * <li>ts is the server timestamp of when the message was received</li>
+ * <li>p is the processing time in ms, i.e. the time the server took before sending the response</li>
+ * <li>a is the measured accuracy of the offset and lag sent by the client</li>
+ * </ul>
+ * On receipt of the response, the client is able to use current time to determine
+ * the total network trip time, from which <b>p</b> is subtracted to determine an approximate
+ * two way network traversal time.
+ * The measured accuracy is used to adjust the assumption that the network is symmetric for traversal time, so:
+ * <pre>
+ * lag = (now - tc - p) / 2 - a;
+ * offset = ts - tc - lag;
+ * </pre>
+ * In order to smooth over any transient fluctuations, the extension keeps a sliding
+ * average of the offsets received, over a number of samples configurable via the <b>maxSamples</b>
+ * parameter:
+ * <pre>
+ * var cometd = ...;
+ * cometd.registerExtension('timesync', new org.cometd.Cometd.TimeSyncExtension({
+ *     maxSamples: 20
+ * }));
+ * cometd.init(...);
+ * var timesync = cometd.getExtension('timesync');
+ * var timeOffset = timesync.getTimeOffset();
+ * </pre>
+ * @param configuration
+ */
+org.cometd.Cometd.TimeSyncExtension = function(configuration)
+{
+    var _cometd;
+    var _maxSamples = configuration && configuration.maxSamples || 10;
+    var _lags = [];
+    var _offsets = [];
+    var _lag = 0;
+    var _offset = 0;
+
+    this.registered = function(name, cometd)
+    {
+        _cometd = cometd;
+        _debug('TimeSyncExtension: executing registration callback');
+    };
+
+    this.unregistered = function()
+    {
+        _debug('TimeSyncExtension: executing unregistration callback');
+        _cometd = null;
+        _lags = [];
+        _offsets = [];
+    };
+
+    this.incoming = function(message)
+    {
+        var channel = message.channel;
+        if (channel && channel.indexOf('/meta/') == 0)
+        {
+            if (message.ext && message.ext.timesync)
+            {
+                var timesync = message.ext.timesync;
+                _debug('TimeSyncExtension: server sent timesync: {}', org.cometd.JSON.toJSON(timesync));
+
+                var now = new Date().getTime();
+                var currentLag = (now - timesync.tc - timesync.p) / 2 - timesync.a;
+                var currentOffset = timesync.ts - timesync.tc - currentLag;
+
+                _lags.push(currentLag);
+                _offsets.push(currentOffset);
+                if (_offsets.length > _maxSamples)
+                {
+                    _offsets.shift();
+                    _lags.shift();
+                }
+
+                var samples = _offsets.length;
+                var lagsSum = 0;
+                var offsetsSum = 0;
+                for (var i = 0; i < samples; ++i)
+                {
+                    lagsSum += _lags[i];
+                    offsetsSum += _offsets[i];
+                }
+                _lag = parseInt((lagsSum / samples).toFixed());
+                _offset = parseInt((offsetsSum / samples).toFixed());
+                _debug('TimeSyncExtension: network lag {} ms, time offset with server {} ms', _lag, _offset);
+            }
+        }
+        return message;
+    };
+
+    this.outgoing = function(message)
+    {
+        var channel = message.channel;
+        if (channel && channel.indexOf('/meta/') == 0)
+        {
+            if (!message.ext) message.ext = {};
+            message.ext.timesync = {
+                tc: new Date().getTime(),
+                l: _lag,
+                o: _offset
+            };
+            _debug('TimeSyncExtension: client sending timesync: {}', org.cometd.JSON.toJSON(message.ext.timesync));
+        }
+        return message;
+    };
+
+    this.getTimeOffset = function()
+    {
+        return _offset;
+    };
+
+    this.getNetworkLag = function()
+    {
+        return _lag;
+    };
+
+    function _debug(text, args)
+    {
+        _cometd._debug(text, args);
+    }
+}
---
+++ cometd-javascript/jquery/src/test/java/org/cometd/javascript/jquery/extension/CometTimeSyncExtensionTest.java
@@ -0,0 +1,67 @@
+package org.cometd.javascript.jquery.extension;
+
+import java.net.URL;
+
+import org.cometd.Bayeux;
+import org.cometd.javascript.jquery.AbstractJQueryCometTest;
+import org.cometd.server.ext.TimesyncExtension;
+import org.testng.annotations.Test;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class CometTimeSyncExtensionTest extends AbstractJQueryCometTest
+{
+    protected void customizeBayeux(Bayeux bayeux)
+    {
+        bayeux.addExtension(new TimesyncExtension());
+    }
+
+    @Test
+    public void testTimeSync() throws Exception
+    {
+        URL timesyncExtensionURL = new URL(contextURL + "/org/cometd/cometd-timesync.js");
+        evaluateURL(timesyncExtensionURL);
+
+        evaluateScript("$.cometd.setLogLevel('debug');");
+        evaluateScript("$.cometd.registerExtension('timesync', new org.cometd.Cometd.TimeSyncExtension());");
+
+        evaluateScript("var inTimeSync = undefined;");
+        evaluateScript("var outTimeSync = undefined;");
+        evaluateScript("$.cometd.registerExtension('test', {" +
+                "incoming: function(message)" +
+                "{" +
+                "    var channel = message.channel;" +
+                "    if (channel && channel.indexOf('/meta/') == 0)" +
+                "    {" +
+                "        inTimeSync = message.ext && message.ext.timesync;" +
+                "    }" +
+                "}," +
+                "outgoing: function(message)" +
+                "{" +
+                "    var channel = message.channel;" +
+                "    if (channel && channel.indexOf('/meta/') == 0)" +
+                "    {" +
+                "        outTimeSync = message.ext && message.ext.timesync;" +
+                "    }" +
+                "}" +
+                "});");
+        evaluateScript("$.cometd.init('" + cometURL + "');");
+        Thread.sleep(500); // Wait for the long poll
+
+        // Both client and server should support timesync
+        Object outTimeSync = get("outTimeSync");
+        assert outTimeSync != null;
+        Object inTimeSync = get("inTimeSync");
+        assert inTimeSync != null;
+
+        evaluateScript("var timesync = $.cometd.getExtension('timesync');");
+        evaluateScript("var networkLag = timesync.getNetworkLag();");
+        evaluateScript("var timeOffset = timesync.getTimeOffset();");
+        int networkLag = ((Number)get("networkLag")).intValue();
+        assert networkLag > 0;
+
+        evaluateScript("$.cometd.disconnect();");
+        Thread.sleep(500); // Wait for the disconnect to return
+    }
+}
