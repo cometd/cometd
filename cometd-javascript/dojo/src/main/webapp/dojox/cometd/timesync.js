efda6f7cf6c61317d679fca96b463bb0a65d1ec4
Greg Wilkins <gregw@intalio.com>
2009-05-11 15:20:00 +0000

    updated timesync extension

--- cometd-javascript/dojo/src/main/webapp/dojox/cometd/timesync.js
+++ cometd-javascript/dojo/src/main/webapp/dojox/cometd/timesync.js
@@ -2,4 +2,5 @@ dojo.provide("dojox.cometd.timesync");
 dojo.require("dojox.cometd");
 dojo.require("org.cometd.TimeSyncExtension");
 
-dojox.cometd.registerExtension('timesync', new org.cometd.TimeSyncExtension());
+dojox.cometd.timesync=new org.cometd.TimeSyncExtension();
+dojox.cometd.registerExtension('timesync', dojox.cometd.timesync);
