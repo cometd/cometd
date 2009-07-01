0068d6fa19e2068bd2794f38ea022e911a0c3aac
sbordet <sbordet@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2009-07-01 08:41:50 +0000

    Fixed application.js in the archetypes.
    git-svn-id: http://svn.cometd.com/trunk@673 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-archetypes/dojo-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
+++ cometd-archetypes/dojo-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
@@ -2,13 +2,24 @@ dojo.require("dojox.cometd");
 
 dojo.addOnLoad(function()
 {
+    var cometd = dojox.cometd;
+    dojo.addOnUnload(function()
+    {
+        cometd.disconnect();
+    });
+
     dojox.cometd.setLogLevel('debug');
     dojox.cometd.addListener('/meta/connect', _onConnect);
 
     var cometURL = location.protocol + "//" + location.host + config.contextPath + "/cometd";
-    dojox.cometd.init({
-        url: cometURL
+    cometd.configure({
+        url: cometURL,
+        logLevel: 'debug'
     });
+
+    cometd.addListener('/meta/connect', _onConnect);
+
+    cometd.handshake();
 
     var _connected = false;
     function _onConnect(message)
--- cometd-archetypes/jquery-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
+++ cometd-archetypes/jquery-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
@@ -2,13 +2,22 @@
 {
     $(document).ready(function()
     {
-        $.cometd.setLogLevel('debug');
-        $.cometd.addListener('/meta/connect', _onConnect);
+        var cometd = $.cometd;
+        // Disconnect when the page unloads
+        $(window).unload(function()
+        {
+            cometd.disconnect();
+        });
 
         var cometURL = location.protocol + "//" + location.host + config.contextPath + "/cometd";
-        $.cometd.init({
-            url: cometURL
+        cometd.configure({
+            url: cometURL,
+            logLevel: 'debug'
         });
+
+        cometd.addListener('/meta/connect', _onConnect);
+
+        cometd.handshake();
 
         var _connected = false;
         function _onConnect(message)
