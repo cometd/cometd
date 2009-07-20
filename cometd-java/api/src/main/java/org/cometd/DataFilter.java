5169a7d52573e255bfc82ce5d03b69e315c2f12a
sbordet <sbordet@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2009-07-20 13:22:55 +0000

    Javadocs.
    git-svn-id: http://svn.cometd.com/trunk@702 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-java/api/src/main/java/org/cometd/DataFilter.java
+++ cometd-java/api/src/main/java/org/cometd/DataFilter.java
@@ -3,7 +3,7 @@
 // ------------------------------------------------------------------------
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at 
+// You may obtain a copy of the License at
 // http://www.apache.org/licenses/LICENSE-2.0
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,19 +15,19 @@
 package org.cometd;
 
 
-/* ------------------------------------------------------------ */
-/** Data Filter
- * Data filters are used to transform data as it is sent to a Channel.
- * 
+/**
+ * Data filters are used to transform data as it is sent to a {@link Channel}.
+ *
+ * @version $Revision: 686 $ $Date: 2009-07-03 11:07:24 +0200 (Fri, 03 Jul 2009) $
  */
 public interface DataFilter
 {
     /**
-     * @param from
-     * @param to TODO
-     * @param data
-     * @return The filtered data.
+     * @param from the {@link Client} that sends the data
+     * @param channel the channel the data is being sent to
+     * @param data the data being sent
+     * @return the transformed data
      * @throws IllegalStateException If the message should be aborted
      */
-    Object filter(Client from, Channel to, Object data) throws IllegalStateException;
+    public Object filter(Client from, Channel channel, Object data) throws IllegalStateException;
 }
