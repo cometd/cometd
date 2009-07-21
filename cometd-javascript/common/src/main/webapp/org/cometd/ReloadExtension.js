002cdb65c97006a2448bf2d2edaf0ded641df9af
Simone Bordet <sbordet@intalio.com>
2009-07-21 10:24:03 +0000

    Made jslint happy.

--- cometd-javascript/common/src/main/webapp/org/cometd/ReloadExtension.js
+++ cometd-javascript/common/src/main/webapp/org/cometd/ReloadExtension.js
@@ -3,8 +3,10 @@
  * $Revision$ $Date: 2009-05-10 13:06:45 +1000 (Sun, 10 May 2009) $
  */
 
-if (typeof dojo != "undefined")
-    dojo.provide("org.cometd.ReloadExtension");
+if (typeof dojo != 'undefined')
+{
+    dojo.provide('org.cometd.ReloadExtension');
+}
 
 if (!org.cometd.COOKIE)
 {
@@ -20,31 +22,35 @@ if (!org.cometd.COOKIE)
 }
 
 /**
- * This extension allows cometd to be reloaded and to reuse
- * the client ID of the former page.
+ * The reload extension allows a page to be loaded (or reloaded)
+ * without having to re-handshake in the new (or reloaded) page,
+ * therefore resuming the existing cometd connection.
  *
- * When the reload() method is called, the state of the connection
- * and subscriptions is stored in a cookie with a short max-age.
- * when the new page is loaded
+ * When the reload() method is called, the state of the cometd
+ * connection and of the cometd subscriptions is stored in a cookie
+ * with a short max-age.
+ * The reload() method must therefore be called by page unload
+ * handlers, often provided by JavaScript toolkits.
+ *
+ * When the page is (re)loaded, this extension checks the cookie
+ * and restores the cometd connection and the cometd subscriptions.
  */
 org.cometd.ReloadExtension = function(configuration)
 {
     var _cometd;
     var _debug;
-    var _state = 
-    {
-    	hsOut:null,  // handshake message Outbound
-	hsIn:null,   // handshake message Inbound
-	subs:{}
+    var _state = {
+        handshakeRequest: null,
+        handshakeResponse: null,
+        subscriptions: {}
     };
-    
     var _cookieMaxAge = configuration && configuration.cookieMaxAge || 5;
 
     function _reload()
     {
-        if (_state && _state.hsIn != null)
+        if (_state && _state.handshakeResponse !== null)
         {
-            org.cometd.COOKIE.set("org.cometd.reload", org.cometd.JSON.toJSON(_state), {'max-age':_cookieMaxAge});
+            org.cometd.COOKIE.set('org.cometd.reload', org.cometd.JSON.toJSON(_state), {'max-age': _cookieMaxAge});
             _state = {};
         }
     }
@@ -60,94 +66,100 @@ org.cometd.ReloadExtension = function(configuration)
     {
         delete _cometd.reload;
         _cometd = null;
-    }
+    };
 
     this.outgoing = function(message)
     {
         var channel = message.channel;
-	
+
         if (channel == '/meta/handshake')
         {
-            // this is a handshake.   Is there a saved response from a prior load?
-            _state.hsOut = message;
-            var cookie = org.cometd.COOKIE.get("org.cometd.reload");
-            if (cookie != null)
+            _state.handshakeRequest = message;
+            _state.subscriptions = {};
+            var cookie = org.cometd.COOKIE.get('org.cometd.reload');
+            // Is there a saved handshake response from a prior load ?
+            if (cookie)
             {
                 try
                 {
-                    org.cometd.COOKIE.set("org.cometd.reload", "", {expires:-1});
-                    _debug(cookie);
+                    org.cometd.COOKIE.set('org.cometd.reload', '', {expires:-1});
+                    _debug('Reload extension found cookie', cookie);
                     var reload = org.cometd.JSON.fromJSON(cookie);
 
-                    // If the handshake matches the save copy, send the prior
-		    // handshake response.
-                    if (reload && reload.hsIn && reload.hsOut &&
-                        org.cometd.JSON.toJSON(message) == org.cometd.JSON.toJSON(reload.hsOut))
+                    // If the handshake matches the save copy, send the prior handshake response.
+                    if (reload && reload.handshakeResponse && reload.handshakeRequest &&
+                        org.cometd.JSON.toJSON(message) == org.cometd.JSON.toJSON(reload.handshakeRequest))
                     {
-                        _debug(reload);
+                        _debug('Reload extension restoring state', reload);
                         setTimeout(function()
                         {
-                            _state.hsIn = reload.hsIn;
-			    _state.subs = reload.subs;
-                            _cometd.receive(reload.hsIn);
-                        }, 1);
+                            _state.handshakeResponse = reload.handshakeResponse;
+                            _state.subscriptions = reload.subscriptions;
+                            _cometd.receive(reload.handshakeResponse);
+                        }, 0);
+                        // This handshake is aborted, as we will replay the prior handshake response
                         return null;
                     }
                 }
                 catch(x)
                 {
-                    _debug(x);
+                    _debug('Reload extension error while trying to restore cookie', x);
                 }
             }
-            _state.subs = {};
         }
         else if (channel == '/meta/subscribe')
         {
-	    // are we already subscribed?
-	    if (_state.subs[message.subscription])
-	    {
-                // consume the subscribe message, as we are already subscribed!
+            // Are we already subscribed ?
+            if (_state.subscriptions[message.subscription])
+            {
+                // Consume the subscribe message, as we are already subscribed
                 setTimeout(function()
                 {
-                    _cometd.receive(
-                    {
-                        channel:'/meta/subscribe',
-                        subscription:message.subscription,
+                    _cometd.receive({
+                        channel: '/meta/subscribe',
+                        subscription: message.subscription,
                         successful: true
                     });
-                }, 1);
+                }, 0);
+
+                _debug('Reload extension will replay subscription to', message.subscription);
+                // This subscription is aborted, as we will replay a previous one
                 return null;
-	    }
+            }
         }
         else if (channel == '/meta/disconnect')
         {
-            _state = {hsOut:null,hsIn:null,subs:{}};
+            _state = {
+                handshakeRequest: null,
+                handshakeResponse: null,
+                subscriptions: {}
+            };
         }
         return message;
     };
 
     this.incoming = function(message)
     {
-        var channel = message.channel;
-	
-    	// save outbound handshake message
-        if (channel == '/meta/handshake' && message.successful)
+        if (message.successful)
         {
-            _state.hsIn = message;
-        }
-	
-	// track subscriptions
-        else if (channel == '/meta/subscribe' && message.successful)
-        {
-            _state.subs[message.subscription] = true;
-        }
-	
-        // track unsubscriptions
-        else if (channel == '/meta/unsubscribe' && message.successful)
-        {
-            delete _state.subs[message.subscription];
+            switch(message.channel)
+            {
+                case '/meta/handshake':
+                    // Save successful handshake response
+                    _state.handshakeResponse = message;
+                    break;
+                case '/meta/subscribe':
+                    // Track subscriptions
+                    _state.subscriptions[message.subscription] = true;
+                    break;
+                case '/meta/unsubscribe':
+                    // Track unsubscriptions
+                    delete _state.subscriptions[message.subscription];
+                    break;
+                default:
+                    break;
+            }
         }
         return message;
     };
-
 };
