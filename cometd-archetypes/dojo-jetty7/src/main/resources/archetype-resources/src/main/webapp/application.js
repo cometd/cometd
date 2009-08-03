28888b26014973f0f7fd300badc6df7064a5f831
sbordet <sbordet@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2009-08-03 14:00:57 +0000

    Updated archetypes javascript to make them pass JSLint.
    git-svn-id: http://svn.cometd.com/trunk@721 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-archetypes/dojo-jetty7/src/main/resources/archetype-resources/src/main/webapp/application.js
+++ cometd-archetypes/dojo-jetty7/src/main/resources/archetype-resources/src/main/webapp/application.js
@@ -2,7 +2,35 @@ dojo.require("dojox.cometd");
 
 dojo.addOnLoad(function()
 {
+    var _connected = false;
+
+    function _connectionSucceeded()
+    {
+        dojo.byId('body').innerHTML = 'Cometd Connection Succeeded';
+    }
+
+    function _connectionBroken()
+    {
+        dojo.byId('body').innerHTML = 'Cometd Connection Broken';
+    }
+
+    function _metaConnect(message)
+    {
+        var wasConnected = _connected;
+        _connected = message.successful === true;
+        if (!wasConnected && _connected)
+        {
+            _connectionSucceeded();
+        }
+        else if (wasConnected && !_connected)
+        {
+            _connectionBroken();
+        }
+    }
+
     var cometd = dojox.cometd;
+
+    // Disconnect when the page unloads
     dojo.addOnUnload(function()
     {
         cometd.disconnect();
@@ -14,18 +42,7 @@ dojo.addOnLoad(function()
         logLevel: 'debug'
     });
 
-    cometd.addListener('/meta/connect', _onConnect);
+    cometd.addListener('/meta/connect', _metaConnect);
 
     cometd.handshake();
-
-    var _connected = false;
-    function _onConnect(message)
-    {
-        var wasConnected = _connected;
-        _connected = message.successful;
-        if (!wasConnected && _connected)
-        {
-            dojo.byId('body').innerHTML = 'Cometd Configured Successfully';
-        }
-    };
 });
