a80d2e8630783176017bd000c86308147e3957d6
Greg Wilkins <gregw@intalio.com>
2009-09-28 03:01:33 +0000

    run.sh

--- cometd-java/client/bin/run.sh
+++ cometd-java/client/bin/run.sh
@@ -2,6 +2,20 @@
 # This run script assumes you have built from source (poor assumption)
 cd `dirname $0`/..
 REPO=$HOME/.m2/repository
-JETTY_VERSION=7.0.0.M1
 
-java -Xmx2048M -Drepo=$REPO -DSTART=bin/start.config -jar $REPO/org/eclipse/jetty/jetty-start/$JETTY_VERSION/jetty-start-$JETTY_VERSION.jar
+echo -n "Jetty Version (from local maven repo) [7.0.0]: "
+read VERSION
+[ "$VERSION" = "" ] && VERSION=7.0.0
+
+CLASSPATH=\
+$REPO/org/eclipse/jetty/jetty-util/$VERSION/jetty-util-$VERSION.jar:\
+$REPO/org/eclipse/jetty/jetty-io/$VERSION/jetty-io-$VERSION.jar:\
+$REPO/org/eclipse/jetty/jetty-http/$VERSION/jetty-http-$VERSION.jar:\
+$REPO/org/eclipse/jetty/jetty-client/$VERSION/jetty-client-$VERSION.jar:\
+$REPO/javax/servlet/servlet-api/2.5/servlet-api-2.5.jar:\
+../api/target/classes:\
+../server/target/classes:\
+target/classes:\
+target/test-classes
+
+java -Xmx2048M -cp $CLASSPATH org.cometd.client.BayeuxLoadGenerator
