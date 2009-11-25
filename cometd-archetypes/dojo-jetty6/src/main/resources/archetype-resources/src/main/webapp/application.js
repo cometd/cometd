32e0214ba8e81867edb1469d363875ecca6f659a
sbordet <sbordet@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2009-11-25 15:38:05 +0000

    Fixed COMETD-34: Add CrossOriginFilter to archetypes and demo.
    git-svn-id: http://svn.cometd.com/trunk@801 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-archetypes/dojo-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
+++ cometd-archetypes/dojo-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
@@ -2,11 +2,27 @@ dojo.require("dojox.cometd");
 
 dojo.addOnLoad(function()
 {
-    var _connected = false;
+    var cometd = dojox.cometd;
 
+    // Idempotent function called every time the connection
+    // with the Bayeux server is (re-)established
+    var _subscription;
     function _connectionSucceeded()
     {
-        dojo.byId('body').innerHTML = 'Cometd Connection Succeeded';
+        dojo.byId('body').innerHTML = '<div>Cometd Connection Succeeded</div>';
+
+        cometd.startBatch();
+        if (_subscription)
+        {
+            cometd.unsubscribe(_subscription);
+        }
+        _subscription = cometd.subscribe('/hello', function(message)
+        {
+            dojo.byId('body').innerHTML += '<div>Server Says: ' + message.data.greeting + '</div>';
+        });
+        // Publish on a service channel since the message is for the server only
+        cometd.publish('/service/hello', { name: 'World' });
+        cometd.endBatch();
     }
 
     function _connectionBroken()
@@ -14,6 +30,8 @@ dojo.addOnLoad(function()
         dojo.byId('body').innerHTML = 'Cometd Connection Broken';
     }
 
+    // Function that manages the connection status with the Bayeux server
+    var _connected = false;
     function _metaConnect(message)
     {
         var wasConnected = _connected;
@@ -27,8 +45,6 @@ dojo.addOnLoad(function()
             _connectionBroken();
         }
     }
-
-    var cometd = dojox.cometd;
 
     // Disconnect when the page unloads
     dojo.addOnUnload(function()
