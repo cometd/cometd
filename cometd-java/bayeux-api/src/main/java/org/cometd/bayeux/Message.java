861c2d92ae4d28e18f28c806acb3883000aa742a
gregw <gregw@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2009-12-10 02:28:12 +0000

    simplifications
    git-svn-id: http://svn.cometd.com/branches/cometd2@831 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/Message.java
+++ cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/Message.java
@@ -5,13 +5,12 @@ import java.util.Map;
 /**
  * @version $Revision$ $Date: 2009-12-08 09:42:45 +1100 (Tue, 08 Dec 2009) $
  */
-public interface Message extends Cloneable
+public interface Message
 {
-    String getClientId();
     String getId();
+    String getClientId();
     String getChannelId();
     Object getData();
-    Map<String, Object> getExt(boolean create);
-    public Message getAssociated();
-    public Object clone();
+    Map<String, Object> getExt();
+    Map<String, Object> getAdvice();
 }
