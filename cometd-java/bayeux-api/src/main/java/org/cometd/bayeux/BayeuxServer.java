61d863f35d4559273157890ba743d7b55fcf6312
Greg Wilkins <gregw@intalio.com>
2009-12-10 02:28:12 +0000

    simplifications

--- cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/BayeuxServer.java
+++ cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/BayeuxServer.java
@@ -1,12 +1,7 @@
 package org.cometd.bayeux;
 
-import java.util.Set;
-
 public interface BayeuxServer extends Bayeux
 {
-    Set<ServerChannel> getChannels();
-    Set<ServerSession> getSessions();
-    
     void addSessionListener(SessionListener listener);
     
     LocalSession newLocalSession(String idHint);
