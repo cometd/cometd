73aabd774075eee605f44fbde77e4cf0aab1f7c1
sbordet <sbordet@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2009-12-21 17:07:41 +0000

    Removed, temporarly, dependency on client packages.
    git-svn-id: http://svn.cometd.com/branches/cometd2@891 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/MessageListener.java
+++ cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/MessageListener.java
@@ -1,13 +1,12 @@
 package org.cometd.bayeux;
 
-import org.cometd.bayeux.client.ClientSession;
 import org.cometd.bayeux.server.ServerSession;
 
 
 /**
  * @version $Revision$ $Date: 2009-12-08 09:57:37 +1100 (Tue, 08 Dec 2009) $
  */
-public interface MessageListener extends ServerSession.Listener,ClientSession.Listener
+public interface MessageListener extends ServerSession.Listener//,ClientSession.Listener TODO
 {
     void onMessage(Message message);
 }
