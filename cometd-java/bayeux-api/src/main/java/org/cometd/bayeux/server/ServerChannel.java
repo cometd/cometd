8a5704d23a72c833cdfbc3722635341d5124824f
gregw <gregw@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2010-01-04 06:06:34 +0000

    revised API
    git-svn-id: http://svn.cometd.com/branches/cometd2@906 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/server/ServerChannel.java
+++ cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/server/ServerChannel.java
@@ -15,11 +15,41 @@ import org.cometd.bayeux.Session;
 public interface ServerChannel extends Channel
 {
     Set<ServerSession> getSubscribers();
+    
     boolean isBroadcast();  // !meta and !service;
     boolean isLazy();
     boolean isPersistent();
     
+    /* ------------------------------------------------------------ */
+    /**
+     * The ServerChannel is wild if it was obtained via an ID ending
+     * with "/*".  Wild channels apply to all direct children of the
+     * channel before the "/*".
+     * @return true if the channel is wild.
+     */
+    boolean isWild();
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * The ServerChannel is deeply wild if it was obtained via an ID ending
+     * with "/**".  Deeply Wild channels apply to all descendants of the
+     * channel before the "/**".
+     * @return true if the channel is deeply wild.
+     */
+    boolean isDeepWild();
+    
+    /* ------------------------------------------------------------ */
+    /** Set lazy channel
+     * @param lazy If true, all messages published to this channel will
+     * be marked as lazy.
+     */
     void setLazy(boolean lazy);
+    
+    /* ------------------------------------------------------------ */
+    /** Set persistent channel
+     * @param persistent If true, the channel will not be removed when 
+     * the last subscription is removed.
+     */
     void setPersistent(boolean persistent);
 
     interface ServerChannelListener extends Channel.ChannelListener
@@ -31,6 +61,7 @@ public interface ServerChannel extends Channel
         void onMessage(ServerMessage.Mutable message);
     }
 
+    /* ------------------------------------------------------------ */
     public interface SubscriptionListener extends ServerChannelListener
     {
         /* ------------------------------------------------------------ */
