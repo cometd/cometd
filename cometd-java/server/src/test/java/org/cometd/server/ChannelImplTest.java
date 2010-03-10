ab2dc406d78cf2d33c107d32e20f1ed25cb6a130
Simone Bordet <sbordet@intalio.com>
2010-03-10 10:58:24 +0000

    Fix for COMETD-84 (Incorrect value of channelCount attribute in JConsole MBean tab).

--- cometd-java/server/src/test/java/org/cometd/server/ChannelImplTest.java
+++ cometd-java/server/src/test/java/org/cometd/server/ChannelImplTest.java
@@ -1,15 +1,13 @@
 /**
- * 
+ *
  */
 package org.cometd.server;
 
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
 
 import junit.framework.TestCase;
-
 import org.cometd.Channel;
 import org.cometd.ChannelBayeuxListener;
 
@@ -21,7 +19,7 @@ public class ChannelImplTest extends TestCase
 {
     private Channel _channel;
     private AbstractBayeux _bayeux;
-    public void setUp() throws Exception 
+    public void setUp() throws Exception
     {
         _bayeux = new BayeuxStub();
     }
@@ -36,24 +34,24 @@ public class ChannelImplTest extends TestCase
            {"/test/123", "[/, /test, /test/123]", "/123", null, "[/, /test, /test/123]"},
            {"/test/123", "[/, /test, /test/123]", "/test", "/test", "[/]"}
         };
-        
+
         for ( String[] test : tests )
         {
             _bayeux.getChannel(test[0], true);
             assertEquals(test[1], _bayeux.getChannels().toString());
-            
+
             Channel removed = _bayeux.removeChannel(test[2]);
             assertEquals(test[3], removed == null? null : removed.toString());
             assertEquals(test[4], _bayeux.getChannels().toString());
         }
     }
-    
+
     public void testChannelSubscriptions () throws Exception
-    {   
+    {
       String[] chans = {"/service", "/service/foo", "/service/foo/bar", "/service/foo/bob"};
       final String[] results = new String[chans.length];
-      
-      
+
+
       class MyChannelBayeuxListener implements ChannelBayeuxListener
       {
           int r = 0;
@@ -69,16 +67,28 @@ public class ChannelImplTest extends TestCase
 
           public void channelRemoved(Channel channel)
           {
-              
+
           }
       }
-      
+
       _bayeux.addListener (new MyChannelBayeuxListener(results));
       _bayeux.getChannel("/service/foo/bar", true);
       _bayeux.getChannel("/service/foo/bob", true);
       assertTrue(Arrays.equals(chans,results));
     }
-    
+
+    public void testChannelCount() throws Exception
+    {
+        assertEquals(1, _bayeux.getChannelCount());
+        _bayeux.getChannel("/root/1", true);
+        assertEquals(3, _bayeux.getChannelCount());
+        _bayeux.getChannel("/root/2", true);
+        assertEquals(4, _bayeux.getChannelCount());
+        _bayeux.getChannel("/root/1/2", true);
+        _bayeux.getChannel("/root/1/A/B", true);
+        assertEquals(7, _bayeux.getChannelCount());
+    }
+
     static class BayeuxStub extends AbstractBayeux
     {
         public BayeuxStub()
@@ -94,10 +104,10 @@ public class ChannelImplTest extends TestCase
 
             _random.setSeed(_random.nextLong()^hashCode()^Runtime.getRuntime().freeMemory());
         }
-         
+
         public ClientImpl newRemoteClient()
         {
             return null;
-        }        
+        }
     }
 }
