af7d6dd518eb00227729a12521d8f92e40b71131
Simone Bordet <sbordet@intalio.com>
2010-05-24 16:47:07 +0000

    Javadocs.

--- cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/Bayeux.java
+++ cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/Bayeux.java
@@ -3,44 +3,49 @@ package org.cometd.bayeux;
 import java.util.List;
 import java.util.Set;
 
-
-/* ------------------------------------------------------------ */
-/** 
- * The Bayeux API.
- * <p>This interface is the common interface for client and server 
- * implementations of Bayeux. 
+/**
+ * <p>The {@link Bayeux} interface is the common API for both client-side and
+ * server-side configuration and usage of the Bayeux object.</p>
+ * <p>The {@link Bayeux} object handles configuration options and a set of
+ * transports that is negotiated with the server.</p>
  */
 public interface Bayeux
 {
-    /* ------------------------------------------------------------ */
-    /** 
-     * @return The set of know transport names
+    /**
+     * @return the set of known transport names of this {@link Bayeux} object.
      */
     Set<String> getKnownTransportNames();
-    
-    
-    /* ------------------------------------------------------------ */
-    /** Get a transport
-     * @param transport transport name or "*" for a Transport representing common options.
-     * @return Mutable Map of transport options or null if unknown transport
+
+    /**
+     * @param transport the transport name
+     * @return the transport with the given name or null
+     * if no such transport exist
      */
     Transport getTransport(String transport);
-    
-    /* ------------------------------------------------------------ */
+
     /**
-     * @return List of transports to be used for sessions in the order 
-     * they will be tried.
+     * @return the ordered list of transport names that will be used in the
+     * negotiation of transports with the other peer.
      */
     List<String> getAllowedTransports();
-    
 
-    /* ------------------------------------------------------------ */
+    /**
+     * @param qualifiedName the configuration option name
+     * @return the configuration option with the given {@code qualifiedName}
+     * @see #setOption(String, Object)
+     * @see #getOptionNames()
+     */
     Object getOption(String qualifiedName);
-    
-    /* ------------------------------------------------------------ */
-    void setOption(String qualifiedName,Object value);
-    
-    /* ------------------------------------------------------------ */
-    Set<String> getOptionNames();
 
+    /**
+     * @param qualifiedName the configuration option name
+     * @param value the configuration option value
+     * @see #getOption(String)
+     */
+    void setOption(String qualifiedName, Object value);
+
+    /**
+     * @return the set of configuration options
+     */
+    Set<String> getOptionNames();
 }
