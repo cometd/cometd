94b2616537e8e5b8fff3e01f9a1546c07acbeaa2
Simone Bordet <sbordet@intalio.com>
2010-05-25 09:06:32 +0000

    Javadocs.

--- cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/Session.java
+++ cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/Session.java
@@ -2,103 +2,90 @@ package org.cometd.bayeux;
 
 import java.util.Set;
 
-
-
-/* ------------------------------------------------------------ */
-/** A Bayeux Session.
- * <p>A bayeux session represents a connection between a bayeux client and 
- * a bayeux server.  This interface is the common base interface for both 
- * server side and client side representations of a session:<ul>
- * <li>If the remote client is in javascript, then only a {@link org.cometd.bayeux.server.ServerSession}
- * instance will exist in Java in the server.
- * <li>If the remote client is in java, then a {@link org.cometd.bayeux.client.ClientSession}
- * instance will exist in the client and a {@link org.cometd.bayeux.server.ServerSession}
- * instance will exist in the server, linked by clientId and the bayeux protocol.
- * <li>If the client is java, but located in the server, then the
- * {@link org.cometd.bayeux.client.ClientSession} instance will an instance
+/**
+ * <p>A Bayeux session represents a connection between a bayeux client and a bayeux server.</p>
+ * <p>This interface is the common base interface for both the server side and the client side
+ * representations of a session:</p>
+ * <ul>
+ * <li>if the remote client is not a Java client, then only a {@link org.cometd.bayeux.server.ServerSession}
+ * instance will exist on the server and represents the remote client.</li>
+ * <li>if the remote client is a Java client, then a {@link org.cometd.bayeux.client.ClientSession}
+ * instance will exist on the client and a {@link org.cometd.bayeux.server.ServerSession}
+ * instance will exist on the server, linked by the same clientId.</li>
+ * <li>if the client is a Java client, but it is located in the server, then the
+ * {@link org.cometd.bayeux.client.ClientSession} instance will be an instance
  * of {@link org.cometd.bayeux.server.LocalSession} and will be associated
- * with a local {@link org.cometd.bayeux.server.ServerSession} instance.
+ * with a {@link org.cometd.bayeux.server.ServerSession} instance.</li>
  * </ul>
- * 
- * </p>
- * <p>Previously this interface was called "Client", but this
- * resulted in confusion between the various representation of the server-side 
- * elements of a "Client".   Thus this entity has been renamed "Session", but
- * for backwards compatibility with the wire protocol, it is identified by 
- * a clientID field withing the messages (see {@link Message#getClientId()}).
- * </p>
  */
 public interface Session
 {
-    /* ------------------------------------------------------------ */
-    /** The ClientId of the session.
+    /**
+     * <p>The clientId of the session.</p>
      * <p>This would more correctly be called a "sessionId", but for
-     * backwards compatibility with the bayeux protocol, it is a clientId
+     * backwards compatibility with the Bayeux protocol, it is a field called "clientId"
      * that identifies a session.
-     * @return A string identifying the current session.
+     * @return the id of this session
      */
     String getId();
 
-    /* ------------------------------------------------------------ */
     /**
-     * @return True if the session is connected
+     * <p>A connected session is a session where the link between the client and the server
+     * has been established.</p>
+     * @return whether the session is connected
+     * @see #disconnect()
      */
     boolean isConnected();
 
-    /* ------------------------------------------------------------ */
-    /** Disconnect the session
-     * 
+    /**
+     * Disconnects this session, ending the link between the client and the server peers.
+     * @see #isConnected()
      */
     void disconnect();
-    
 
-    /* ------------------------------------------------------------ */
-    /** Set a session attribute.
+    /**
+     * <p>Sets a named session attribute value.</p>
      * <p>Session attributes are convenience data that allows arbitrary
-     * application data to be associated with a session.
-     * @param name The attribute name
-     * @param value The attribute value
+     * application data to be associated with a session.</p>
+     * @param name the attribute name
+     * @param value the attribute value
      */
-    void setAttribute(String name,Object value);
-    
-    /* ------------------------------------------------------------ */
-    /** Get a named attribute
-     * @param name The name of the attribute
-     * @return The attribute value or null if not set.
+    void setAttribute(String name, Object value);
+
+    /**
+     * <p>Retrieves the value of named session attribute.</p>
+     * @param name the name of the attribute
+     * @return the attribute value or null if the attribute is not present
      */
     Object getAttribute(String name);
-    
-    /* ------------------------------------------------------------ */
-    /** Get Attribute names.
-     * @return Set of known session attribute names
+
+    /**
+     * @return the list of session attribute names.
      */
     Set<String> getAttributeNames();
-    
-    /* ------------------------------------------------------------ */
+
     /**
-     * Remove a session attribute
-     * @param name The name of the attribute
-     * @return the previous value of the attribute
+     * <p>Removes a named session attribute.</p>
+     * @param name the name of the attribute
+     * @return the value of the attribute
      */
     Object removeAttribute(String name);
 
-    /* ------------------------------------------------------------ */
-    /** Run a Runnable in a batch.
+    /**
+     * <p>Executes the given command in a batch so that any Bayeux message sent
+     * by the command (via the Bayeux API) is queued up until the end of the
+     * command and then all messages are sent at once.</p>
      * @param batch the Runnable to run as a batch
      */
     void batch(Runnable batch);
 
-    /* ------------------------------------------------------------ */
     /**
      * @deprecated use {@link #batch(Runnable)}
      */
     void endBatch();
-    
-    /* ------------------------------------------------------------ */
+
     /**
      * @deprecated use {@link #batch(Runnable)}
      */
     void startBatch();
-    
-    
 }
