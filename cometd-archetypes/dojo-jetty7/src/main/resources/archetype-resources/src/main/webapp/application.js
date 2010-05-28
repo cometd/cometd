1cfc4cba85374ce7c7e2769407ba4a0fbd78cabb
sbordet <sbordet@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2010-05-28 14:04:18 +0000

    Final updates of the archetypes.
    git-svn-id: http://svn.cometd.com/branches/cometd2@1233 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-archetypes/dojo-jetty7/src/main/resources/archetype-resources/src/main/webapp/application.js
+++ cometd-archetypes/dojo-jetty7/src/main/resources/archetype-resources/src/main/webapp/application.js
@@ -4,42 +4,37 @@ dojo.addOnLoad(function()
 {
     var cometd = dojox.cometd;
 
-    // Idempotent function called every time the connection
-    // with the Bayeux server is (re-)established
-    var _subscription;
-    function _connectionSucceeded()
+    function _connectionEstablished()
     {
-        dojo.byId('body').innerHTML = '<div>Cometd Connection Succeeded</div>';
-
-        cometd.batch(function()
-        {
-            if (_subscription)
-            {
-                cometd.unsubscribe(_subscription);
-            }
-            _subscription = cometd.subscribe('/hello', function(message)
-            {
-                dojo.byId('body').innerHTML += '<div>Server Says: ' + message.data.greeting + '</div>';
-            });
-            // Publish on a service channel since the message is for the server only
-            cometd.publish('/service/hello', { name: 'World' });
-        });
+        dojo.byId('body').innerHTML += '<div>CometD Connection Established</div>';
     }
 
     function _connectionBroken()
     {
-        dojo.byId('body').innerHTML = 'Cometd Connection Broken';
+        dojo.byId('body').innerHTML += '<div>CometD Connection Broken</div>';
+    }
+
+    function _connectionClosed()
+    {
+        dojo.byId('body').innerHTML += '<div>CometD Connection Closed</div>';
     }
 
     // Function that manages the connection status with the Bayeux server
     var _connected = false;
     function _metaConnect(message)
     {
+        if (cometd.isDisconnected())
+        {
+            _connected = false;
+            _connectionClosed();
+            return;
+        }
+
         var wasConnected = _connected;
         _connected = message.successful === true;
         if (!wasConnected && _connected)
         {
-            _connectionSucceeded();
+            _connectionEstablished();
         }
         else if (wasConnected && !_connected)
         {
@@ -47,10 +42,28 @@ dojo.addOnLoad(function()
         }
     }
 
+    // Function invoked when first contacting the server and
+    // when the server has lost the state of this client
+    function _metaHandshake(handshake)
+    {
+        if (handshake.successful === true)
+        {
+            cometd.batch(function()
+            {
+                cometd.subscribe('/hello', function(message)
+                {
+                    dojo.byId('body').innerHTML += '<div>Server Says: ' + message.data.greeting + '</div>';
+                });
+                // Publish on a service channel since the message is for the server only
+                cometd.publish('/service/hello', { name: 'World' });
+            });
+        }
+    }
+
     // Disconnect when the page unloads
     dojo.addOnUnload(function()
     {
-        cometd.disconnect();
+        cometd.disconnect(true);
     });
 
     var cometURL = location.protocol + "//" + location.host + config.contextPath + "/cometd";
@@ -59,6 +72,7 @@ dojo.addOnLoad(function()
         logLevel: 'debug'
     });
 
+    cometd.addListener('/meta/handshake', _metaHandshake);
     cometd.addListener('/meta/connect', _metaConnect);
 
     cometd.handshake();
