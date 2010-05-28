8125bff789360de09c605dab036c8db7c457d850
Simone Bordet <sbordet@intalio.com>
2010-05-28 13:27:07 +0000

    Second pass at upgrading the archetypes.

--- cometd-archetypes/jquery-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
+++ cometd-archetypes/jquery-jetty6/src/main/resources/archetype-resources/src/main/webapp/application.js
@@ -4,42 +4,37 @@
 
     $(document).ready(function()
     {
-        // Idempotent function called every time the connection
-        // with the Bayeux server is (re-)established
-        var _subscription;
-        function _connectionSucceeded()
+        function _connectionEstablished()
         {
-            $('#body').empty().append('<div>Cometd Connection Succeeded</div>');
-
-            cometd.batch(function()
-            {
-                if (_subscription)
-                {
-                    cometd.unsubscribe(_subscription);
-                }
-                _subscription = cometd.subscribe('/hello', function(message)
-                {
-                    $('#body').append('<div>Server Says: ' + message.data.greeting + '</div>');
-                });
-                // Publish on a service channel since the message is for the server only
-                cometd.publish('/service/hello', { name: 'World' });
-            });
+            $('#body').append('<div>CometD Connection Established</div>');
         }
 
         function _connectionBroken()
         {
-            $('#body').empty().html('Cometd Connection Broken');
+            $('#body').append('<div>CometD Connection Broken</div>');
+        }
+
+        function _connectionClosed()
+        {
+            $('#body').append('<div>CometD Connection Closed</div>');
         }
 
         // Function that manages the connection status with the Bayeux server
         var _connected = false;
         function _metaConnect(message)
         {
+            if (cometd.isDisconnected())
+            {
+                _connected = false;
+                _connectionClosed();
+                return;
+            }
+
             var wasConnected = _connected;
             _connected = message.successful === true;
             if (!wasConnected && _connected)
             {
-                _connectionSucceeded();
+                _connectionEstablished();
             }
             else if (wasConnected && !_connected)
             {
@@ -47,10 +42,28 @@
             }
         }
 
+        // Function invoked when first contacting the server and
+        // when the server has lost the state of this client
+        function _metaHandshake(handshake)
+        {
+            if (handshake.successful === true)
+            {
+                cometd.batch(function()
+                {
+                    cometd.subscribe('/hello', function(message)
+                    {
+                        $('#body').append('<div>Server Says: ' + message.data.greeting + '</div>');
+                    });
+                    // Publish on a service channel since the message is for the server only
+                    cometd.publish('/service/hello', { name: 'World' });
+                });
+            }
+        }
+
         // Disconnect when the page unloads
         $(window).unload(function()
         {
-            cometd.disconnect();
+            cometd.disconnect(true);
         });
 
         var cometURL = location.protocol + "//" + location.host + config.contextPath + "/cometd";
@@ -59,6 +72,7 @@
             logLevel: 'debug'
         });
 
+        cometd.addListener('/meta/handshake', _metaHandshake);
         cometd.addListener('/meta/connect', _metaConnect);
 
         cometd.handshake();
