983f30b2cfd7dc2bacf953494000ef51a99088da
Simone Bordet <sbordet@intalio.com>
2010-06-22 15:21:33 +0000

    Fixes COMETD-127 (BayeuxClient does not remember cookies).

--- cometd-java/cometd-java-client/src/main/java/org/cometd/client/transport/LongPollingTransport.java
+++ cometd-java/cometd-java-client/src/main/java/org/cometd/client/transport/LongPollingTransport.java
@@ -1,8 +1,12 @@
 package org.cometd.client.transport;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,8 +16,11 @@ import org.cometd.client.BayeuxClient;
 import org.eclipse.jetty.client.ContentExchange;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.HttpExchange;
+import org.eclipse.jetty.http.HttpHeaders;
 import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.io.Buffer;
 import org.eclipse.jetty.io.ByteArrayBuffer;
+import org.eclipse.jetty.util.QuotedStringTokenizer;
 import org.eclipse.jetty.util.ajax.JSON;
 
 /**
@@ -126,6 +133,60 @@ public class LongPollingTransport extends ClientTransport
         protected void onRequestCommitted() throws IOException
         {
             _listener.onSending(_messages);
+        }
+
+        @Override
+        protected void onResponseHeader(Buffer name, Buffer value) throws IOException
+        {
+            super.onResponseHeader(name, value);
+            int headerName = HttpHeaders.CACHE.getOrdinal(name);
+            if (headerName == HttpHeaders.SET_COOKIE_ORDINAL)
+            {
+                QuotedStringTokenizer tokenizer = new QuotedStringTokenizer(value.toString(), "=;", false, false);
+                tokenizer.setSingle(false);
+
+                String cookieName = null;
+                if (tokenizer.hasMoreTokens())
+                    cookieName = tokenizer.nextToken();
+
+                String cookieValue = null;
+                if (tokenizer.hasMoreTokens())
+                    cookieValue = tokenizer.nextToken();
+
+                int maxAge = -1;
+
+                if (cookieName != null && cookieValue != null)
+                {
+                    while (tokenizer.hasMoreTokens())
+                    {
+                        String token = tokenizer.nextToken();
+                        if ("Expires".equalsIgnoreCase(token))
+                        {
+                            try
+                            {
+                                Date date = new SimpleDateFormat("EEE, dd-MMM-yy HH:mm:ss 'GMT'").parse(tokenizer.nextToken());
+                                Long maxAgeValue = TimeUnit.MILLISECONDS.toSeconds(date.getTime() - System.currentTimeMillis());
+                                maxAge = maxAgeValue > 0 ? maxAgeValue.intValue() : 0;
+                            }
+                            catch (ParseException ignored)
+                            {
+                            }
+                        }
+                        else if ("Max-Age".equalsIgnoreCase(token))
+                        {
+                            try
+                            {
+                                maxAge = Integer.parseInt(tokenizer.nextToken());
+                            }
+                            catch (NumberFormatException ignored)
+                            {
+                            }
+                        }
+                    }
+
+                    _bayeuxClient.setCookie(cookieName, cookieValue, maxAge);
+                }
+            }
         }
 
         @Override
