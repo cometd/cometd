80b377e49ad6608b7259dfade3e500013819dc0a
sbordet <sbordet@3f0894df-830e-0410-bb9a-a3a9c902c5bb>
2010-10-15 14:00:36 +0000

    Cleanup, removed unused stuff.
    git-svn-id: http://svn.cometd.com/trunk@1478 3f0894df-830e-0410-bb9a-a3a9c902c5bb

--- cometd-java/cometd-java-common/src/main/java/org/cometd/common/HashMapMessage.java
+++ cometd-java/cometd-java-common/src/main/java/org/cometd/common/HashMapMessage.java
@@ -10,186 +10,117 @@ import org.eclipse.jetty.util.ajax.JSON;
 
 public class HashMapMessage extends HashMap<String, Object> implements Message.Mutable, JSON.Generator
 {
-    private static JSON __json=new JSON();
+    private static JSON __json = new JSON();
 
-    private Message _associated;
-    private boolean _lazy=false;
-
-    /* ------------------------------------------------------------ */
-    public HashMapMessage()
-    {
-    }
-
-    /* ------------------------------------------------------------ */
     public void addJSON(Appendable buffer)
     {
         try
         {
             buffer.append(getJSON());
         }
-        catch(IOException e)
+        catch (IOException e)
         {
             throw new RuntimeException(e);
         }
     }
 
-    /* ------------------------------------------------------------ */
-    @Override
-    public void clear()
-    {
-        _lazy=false;
-        super.clear();
-    }
-
-    /* ------------------------------------------------------------ */
     public Map<String, Object> getAdvice()
     {
         return (Map<String, Object>)get(ADVICE_FIELD);
     }
 
-    /* ------------------------------------------------------------ */
-    public Message getAssociated()
-    {
-        return _associated;
-    }
-
-    /* ------------------------------------------------------------ */
     public String getChannel()
     {
         return (String)get(CHANNEL_FIELD);
     }
 
-    /* ------------------------------------------------------------ */
     public String getClientId()
     {
         return (String)get(CLIENT_ID_FIELD);
     }
 
-    /* ------------------------------------------------------------ */
     public Object getData()
     {
         return get(DATA_FIELD);
     }
 
-    /* ------------------------------------------------------------ */
-    public Map<String,Object> getDataAsMap()
+    public Map<String, Object> getDataAsMap()
     {
-        return (Map<String,Object>)get(DATA_FIELD);
+        return (Map<String, Object>)get(DATA_FIELD);
     }
 
-    /* ------------------------------------------------------------ */
     public Map<String, Object> getExt()
     {
         return (Map<String, Object>)get(EXT_FIELD);
     }
 
-    /* ------------------------------------------------------------ */
     public String getId()
     {
         return (String)get(ID_FIELD);
     }
 
-    /* ------------------------------------------------------------ */
     public String getJSON()
     {
-        Appendable buf=new StringBuilder(__json.getStringBufferSize());
-        __json.appendMap(buf,this);
+        Appendable buf = new StringBuilder(__json.getStringBufferSize());
+        __json.appendMap(buf, this);
         return buf.toString();
     }
 
-    /* ------------------------------------------------------------ */
     public Map<String, Object> getAdvice(boolean create)
     {
-        Map<String, Object> advice=getAdvice();
-        if (create && advice==null)
+        Map<String, Object> advice = getAdvice();
+        if (create && advice == null)
         {
-            advice = new HashMap<String,Object>();
-            put(ADVICE_FIELD,advice);
+            advice = new HashMap<String, Object>();
+            put(ADVICE_FIELD, advice);
         }
         return advice;
     }
 
-    /* ------------------------------------------------------------ */
     public Map<String, Object> getDataAsMap(boolean create)
     {
-        Map<String, Object> data=(Map<String,Object>)getData();
-        if (create && data==null)
+        @SuppressWarnings("unchecked")
+        Map<String, Object> data = (Map<String, Object>)getData();
+        if (create && data == null)
         {
-            data = new HashMap<String,Object>();
-            put(DATA_FIELD,data);
+            data = new HashMap<String, Object>();
+            put(DATA_FIELD, data);
         }
         return data;
     }
 
-    /* ------------------------------------------------------------ */
-    public Map<String,Object> getExt(boolean create)
+    public Map<String, Object> getExt(boolean create)
     {
-        Object ext=getExt();
-        if (ext==null && !create)
+        Object ext = getExt();
+        if (ext == null && !create)
             return null;
 
         if (ext instanceof Map)
-            return (Map<String,Object>)ext;
+            return (Map<String, Object>)ext;
 
         if (ext instanceof JSON.Literal)
         {
-            ext=__json.fromJSON(ext.toString());
-            put(EXT_FIELD,ext);
-            return (Map<String,Object>)ext;
+            ext = __json.fromJSON(ext.toString());
+            put(EXT_FIELD, ext);
+            return (Map<String, Object>)ext;
         }
 
-        ext=new HashMap<String,Object>();
-        put(EXT_FIELD,ext);
-        return (Map<String,Object>)ext;
+        ext = new HashMap<String, Object>();
+        put(EXT_FIELD, ext);
+        return (Map<String, Object>)ext;
     }
 
-    /* ------------------------------------------------------------ */
-    /**
-     * Lazy messages are queued but do not wake up waiting clients.
-     *
-     * @return true if message is lazy
-     */
-    public boolean isLazy()
-    {
-        return _lazy;
-    }
-
-
-    /* ------------------------------------------------------------ */
-    /**
-     * @return True if the message is for a Meta channel
-     */
     public boolean isMeta()
     {
         return ChannelId.isMeta(getChannel());
     }
 
-    /* ------------------------------------------------------------ */
     public boolean isSuccessful()
     {
-        Boolean value=(Boolean)get(Message.SUCCESSFUL_FIELD);
+        Boolean value = (Boolean)get(Message.SUCCESSFUL_FIELD);
         return value != null && value;
     }
 
-    /* ------------------------------------------------------------ */
-    public void setAssociated(Message associated)
-    {
-        _associated=associated;
-    }
-
-    /* ------------------------------------------------------------ */
-    /**
-     * Lazy messages are queued but do not wake up waiting clients.
-     *
-     * @param lazy
-     *            true if message is lazy
-     */
-    public void setLazy(boolean lazy)
-    {
-        _lazy=lazy;
-    }
-
-    /* ------------------------------------------------------------ */
     public String toString()
     {
         return getJSON();
@@ -202,28 +133,21 @@ public class HashMapMessage extends HashMap<String, Object> implements Message.M
 
     public void setClientId(String clientId)
     {
-        put(CLIENT_ID_FIELD,clientId);
+        put(CLIENT_ID_FIELD, clientId);
     }
 
     public void setData(Object data)
     {
-        put(DATA_FIELD,data);
+        put(DATA_FIELD, data);
     }
 
     public void setId(String id)
     {
-        put(ID_FIELD,id);
+        put(ID_FIELD, id);
     }
 
     public void setSuccessful(boolean successful)
     {
-        put(SUCCESSFUL_FIELD, successful ?Boolean.TRUE:Boolean.FALSE);
+        put(SUCCESSFUL_FIELD, successful);
     }
-
-
-    public Message asImmutable()
-    {
-        return this;
-    }
-
 }
