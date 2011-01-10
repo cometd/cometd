b974220903d3bfb37d917595f961be46a63d1881
Simone Bordet <sbordet@intalio.com>
2011-01-10 17:52:55 +0000

    Fixes COMETD-206 (Add Spring archetypes).

---
+++ cometd-archetypes/spring-dojo-jetty7/src/main/resources/archetype-resources/src/main/java/BayeuxInitializer.java
@@ -0,0 +1,74 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.java.annotation.ServerAnnotationProcessor;
+import org.cometd.server.BayeuxServerImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletContextAware;
+
+@Component
+public class BayeuxInitializer implements DestructionAwareBeanPostProcessor, ServletContextAware
+{
+    private BayeuxServer bayeuxServer;
+    private ServerAnnotationProcessor processor;
+
+    @Inject
+    private void setBayeuxServer(BayeuxServer bayeuxServer)
+    {
+        this.bayeuxServer = bayeuxServer;
+    }
+
+    @PostConstruct
+    private void init()
+    {
+        this.processor = ServerAnnotationProcessor.get(bayeuxServer);
+    }
+
+    @PreDestroy
+    private void destroy()
+    {
+        this.processor.close();
+    }
+
+    public Object postProcessBeforeInitialization(Object bean, String name) throws BeansException
+    {
+        processor.processDependencies(bean);
+        processor.processConfigurations(bean);
+        processor.processCallbacks(bean);
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String name) throws BeansException
+    {
+        return bean;
+    }
+
+    public void postProcessBeforeDestruction(Object bean, String name) throws BeansException
+    {
+        processor.deprocessCallbacks(bean);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public BayeuxServer bayeuxServer()
+    {
+        BayeuxServerImpl bean = new BayeuxServerImpl();
+        bean.setOption(BayeuxServerImpl.LOG_LEVEL, "3");
+        return bean;
+    }
+
+    public void setServletContext(ServletContext servletContext)
+    {
+        servletContext.setAttribute(BayeuxServer.ATTRIBUTE, bayeuxServer);
+    }
+}
---
+++ cometd-archetypes/spring-dojo-jetty7/src/main/resources/archetype-resources/src/main/webapp/index.jsp
@@ -0,0 +1,25 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <script type="text/javascript" src="${symbol_dollar}{pageContext.request.contextPath}/dojo/dojo.js.uncompressed.js"></script>
+    <script type="text/javascript" src="application.js"></script>
+    <%--
+    The reason to use a JSP is that it is very easy to obtain server-side configuration
+    information (such as the contextPath) and pass it to the JavaScript environment on the client.
+    --%>
+    <script type="text/javascript">
+        var config = {
+            contextPath: '${symbol_dollar}{pageContext.request.contextPath}'
+        };
+    </script>
+</head>
+<body>
+
+    <div id="body"></div>
+
+</body>
+</html>
