427ff5a4a7bc8de8672edc1c50e8eedc75d59e4a
Simone Bordet <sbordet@intalio.com>
2011-05-30 17:36:58 +0200

    Improved javadocs and code cleanups.

--- cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/ChannelId.java
+++ cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/ChannelId.java
@@ -19,80 +19,127 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Holder of a channel ID broken into path segments
+ * <p>Reification of a {@link Channel#getId() channel id} with methods to test properties
+ * and compare with other {@link ChannelId}s.</p>
+ * <p>A {@link ChannelId} breaks the channel id into path segments so that, for example,
+ * {@code /foo/bar} breaks into {@code ["foo","bar"]}.</p>
+ * <p>{@link ChannelId} can be wild, when they end with one or two wild characters {@code "*"};
+ * a {@link ChannelId} is shallow wild if it ends with one wild character (for example {@code /foo/bar/*})
+ * and deep wild if it ends with two wild characters (for example {@code /foo/bar/**}).</p>
  */
 public class ChannelId
 {
-    public final static String WILD="*";
-    public final static String DEEPWILD="**";
+    public final static String WILD = "*";
+    public final static String DEEPWILD = "**";
 
-    private final String  _name;
+    private final String _id;
     private final String[] _segments;
     private final int _wild;
     private final List<String> _wilds;
     private final String _parent;
 
-    public ChannelId(String name)
+    /**
+     * Constructs a new {@code ChannelId} with the given id
+     *
+     * @param id the channel id in string form
+     */
+    public ChannelId(String id)
     {
-        _name=name;
-        if (name == null || name.length() == 0 || name.charAt(0) != '/' || "/".equals(name))
-            throw new IllegalArgumentException(name);
+        if (id == null || id.length() == 0 || id.charAt(0) != '/' || "/".equals(id))
+            throw new IllegalArgumentException("Invalid channel id: " + id);
 
-        String[] wilds;
+        id = id.trim();
+        if (id.charAt(id.length() - 1) == '/')
+            id = id.substring(0, id.length() - 1);
 
-        if (name.charAt(name.length() - 1) == '/')
-            name=name.substring(0,name.length() - 1);
+        _id = id;
 
-        _segments=name.substring(1).split("/");
-        wilds=new String[_segments.length+1];
-        StringBuilder b=new StringBuilder();
+        _segments = id.substring(1).split("/");
+        String[] wilds = new String[_segments.length + 1];
+        StringBuilder b = new StringBuilder();
         b.append('/');
 
-        for (int i=0;i<_segments.length;i++)
+        for (int i = 0; i < _segments.length; ++i)
         {
-            if (_segments[i]==null || _segments[i].length()==0)
-                throw new IllegalArgumentException(name);
+            if (_segments[i] == null || _segments[i].length() == 0)
+                throw new IllegalArgumentException("Invalid channel id: " + id);
 
-            if (i>0)
-                b.append(_segments[i-1]).append('/');
-            wilds[_segments.length-i]=b+"**";
+            if (i > 0)
+                b.append(_segments[i - 1]).append('/');
+
+            wilds[_segments.length - i] = b + "**";
         }
-        wilds[0]=b+"*";
-        _parent=_segments.length==1?null:b.substring(0,b.length()-1);
+        wilds[0] = b + "*";
+        _parent = _segments.length == 1 ? null : b.substring(0, b.length() - 1);
 
         if (_segments.length == 0)
-            _wild=0;
+            _wild = 0;
         else if (WILD.equals(_segments[_segments.length - 1]))
-            _wild=1;
+            _wild = 1;
         else if (DEEPWILD.equals(_segments[_segments.length - 1]))
-            _wild=2;
+            _wild = 2;
         else
-            _wild=0;
+            _wild = 0;
 
-        if (_wild==0)
-            _wilds=Collections.unmodifiableList(Arrays.asList(wilds));
+        if (_wild == 0)
+            _wilds = Collections.unmodifiableList(Arrays.asList(wilds));
         else
-            _wilds=Collections.emptyList();
+            _wilds = Collections.emptyList();
     }
 
+    /**
+     * @return whether this {@code ChannelId} is either {@link #isShallowWild() shallow wild}
+     *         or {@link #isDeepWild() deep wild}
+     */
     public boolean isWild()
     {
         return _wild > 0;
     }
 
+    /**
+     * <p>Shallow wild {@code ChannelId}s end with a single wild character {@code "*"}
+     * and {@link #matches(ChannelId) match} non wild channels with
+     * the same {@link #depth() depth}.</p>
+     * <p>Example: {@code /foo/*} matches {@code /foo/bar}, but not {@code /foo/bar/baz}.</p>
+     *
+     * @return whether this {@code ChannelId} is a shallow wild channel id
+     */
+    public boolean isShallowWild()
+    {
+        return isWild() && !isDeepWild();
+    }
+
+    /**
+     * <p>Deep wild {@code ChannelId}s end with a double wild character "**"
+     * and {@link #matches(ChannelId) match} non wild channels with
+     * the same or greater {@link #depth() depth}.</p>
+     * <p>Example: {@code /foo/**} matches {@code /foo/bar} and {@code /foo/bar/baz}.</p>
+     *
+     * @return whether this {@code ChannelId} is a deep wild channel id
+     */
     public boolean isDeepWild()
     {
         return _wild > 1;
     }
 
+    /**
+     * <p>A {@code ChannelId} is a meta {@code ChannelId} if it starts with {@code "/meta/"}.</p>
+     *
+     * @return whether the first segment is "meta"
+     */
     public boolean isMeta()
     {
-        return _segments.length>0 && "meta".equals(_segments[0]);
+        return _segments.length > 0 && "meta".equals(_segments[0]);
     }
 
+    /**
+     * <p>A {@code ChannelId} is a service {@code ChannelId} if it starts with {@code "/service/"}.</p>
+     *
+     * @return whether the first segment is "service"
+     */
     public boolean isService()
     {
-        return _segments.length>0 && "service".equals(_segments[0]);
+        return _segments.length > 0 && "service".equals(_segments[0]);
     }
 
     @Override
@@ -101,79 +148,95 @@ public class ChannelId
         if (this == obj)
             return true;
 
-        if (obj instanceof ChannelId)
-        {
-            ChannelId id = (ChannelId)obj;
-            if (id.depth()==depth())
-            {
-                for (int i=id.depth();i-->0;)
-                    if (!id.getSegment(i).equals(getSegment(i)))
-                        return false;
-                return true;
-            }
-        }
+        if (!(obj instanceof ChannelId))
+            return false;
 
-        return false;
-    }
-
-    /* ------------------------------------------------------------ */
-    /** Match channel IDs with wildcard support
-     * @param name
-     * @return true if this channelID matches the passed channel ID. If this channel is wild, then matching is wild.
-     * If the passed channel is wild, then it is the same as an equals call.
-     */
-    public boolean matches(ChannelId name)
-    {
-        if (name.isWild())
-            return equals(name);
-
-        switch(_wild)
-        {
-            case 0:
-                return equals(name);
-            case 1:
-                if (name._segments.length != _segments.length)
-                    return false;
-                for (int i=_segments.length - 1; i-- > 0;)
-                    if (!_segments[i].equals(name._segments[i]))
-                        return false;
-                return true;
-
-            case 2:
-                if (name._segments.length < _segments.length)
-                    return false;
-                for (int i=_segments.length - 1; i-- > 0;)
-                    if (!_segments[i].equals(name._segments[i]))
-                        return false;
-                return true;
-        }
-        return false;
+        ChannelId that = (ChannelId)obj;
+        return _id.equals(that._id);
     }
 
     @Override
     public int hashCode()
     {
-        return _name.hashCode();
+        return _id.hashCode();
+    }
+
+    /**
+     * <p>Tests whether this {@code ChannelId} matches the given {@code ChannelId}.</p>
+     * <p>If the given {@code ChannelId} is {@link #isWild() wild},
+     * then it matches only if it is equal to this {@code ChannelId}.</p>
+     * <p>If this {@code ChannelId} is non-wild,
+     * then it matches only if it is equal to the given {@code ChannelId}.</p>
+     * <p>Otherwise, this {@code ChannelId} is either shallow or deep wild, and
+     * matches {@code ChannelId}s with the same number of segments (if it is
+     * shallow wild), or {@code ChannelId}s with the same or a greater number of
+     * segments (if it is deep wild).</p>
+     *
+     * @param channelId the channelId to match
+     * @return true if this {@code ChannelId} matches the given {@code ChannelId}
+     */
+    public boolean matches(ChannelId channelId)
+    {
+        if (channelId.isWild())
+            return equals(channelId);
+
+        switch (_wild)
+        {
+            case 0:
+            {
+                return equals(channelId);
+            }
+            case 1:
+            {
+                if (channelId._segments.length != _segments.length)
+                    return false;
+                for (int i = _segments.length - 1; i-- > 0; )
+                    if (!_segments[i].equals(channelId._segments[i]))
+                        return false;
+                return true;
+            }
+            case 2:
+            {
+                if (channelId._segments.length < _segments.length)
+                    return false;
+                for (int i = _segments.length - 1; i-- > 0; )
+                    if (!_segments[i].equals(channelId._segments[i]))
+                        return false;
+                return true;
+            }
+            default:
+            {
+                throw new IllegalStateException();
+            }
+        }
     }
 
     @Override
     public String toString()
     {
-        return _name;
+        return _id;
     }
 
+    /**
+     * @return how many segments this {@code ChannelId} is made of
+     * @see #getSegment(int)
+     */
     public int depth()
     {
         return _segments.length;
     }
 
-    /* ------------------------------------------------------------ */
+    /**
+     * @param id the channel to test
+     * @return whether this {@code ChannelId} is an ancestor of the given {@code ChannelId}
+     * @see #isParentOf(ChannelId)
+     */
     public boolean isAncestorOf(ChannelId id)
     {
         if (isWild() || depth() >= id.depth())
             return false;
 
-        for (int i=_segments.length; i--> 0;)
+        for (int i = _segments.length; i-- > 0; )
         {
             if (!_segments[i].equals(id._segments[i]))
                 return false;
@@ -181,13 +244,17 @@ public class ChannelId
         return true;
     }
 
-    /* ------------------------------------------------------------ */
+    /**
+     * @param id the channel to test
+     * @return whether this {@code ChannelId} is the parent of the given {@code ChannelId}
+     * @see #isAncestorOf(ChannelId)
+     */
     public boolean isParentOf(ChannelId id)
     {
-        if (isWild() || depth() != id.depth()-1)
+        if (isWild() || depth() != id.depth() - 1)
             return false;
 
-        for (int i=_segments.length; i--> 0;)
+        for (int i = _segments.length; i-- > 0; )
         {
             if (!_segments[i].equals(id._segments[i]))
                 return false;
@@ -195,13 +262,20 @@ public class ChannelId
         return true;
     }
 
-    /* ------------------------------------------------------------ */
+    /**
+     * @return the {@code ChannelId} parent of this {@code ChannelId}
+     * @see #isParentOf(ChannelId)
+     */
     public String getParent()
     {
         return _parent;
     }
 
-    /* ------------------------------------------------------------ */
+    /**
+     * @param i the segment index
+     * @return the i-nth segment of this channel, or null if no such segment exist
+     * @see #depth()
+     */
     public String getSegment(int i)
     {
         if (i >= _segments.length)
@@ -209,23 +283,36 @@ public class ChannelId
         return _segments[i];
     }
 
-    /* ------------------------------------------------------------ */
     /**
      * @return The list of wilds channels that match this channel, or
-     * the empty list if this channel is already wild.
+     *         the empty list if this channel is already wild.
      */
     public List<String> getWilds()
     {
         return _wilds;
     }
 
+    /**
+     * <p>Helper method to test if the string form of a {@code ChannelId}
+     * represents a {@link #isMeta() meta} {@code ChannelId}.</p>
+     *
+     * @param channelId the channel id to test
+     * @return whether the given channel id is a meta channel id
+     */
     public static boolean isMeta(String channelId)
     {
-        return channelId!=null && channelId.startsWith("/meta/");
+        return channelId != null && channelId.startsWith("/meta/");
     }
 
+    /**
+     * <p>Helper method to test if the string form of a {@code ChannelId}
+     * represents a {@link #isService()} service} {@code ChannelId}.</p>
+     *
+     * @param channelId the channel id to test
+     * @return whether the given channel id is a service channel id
+     */
     public static boolean isService(String channelId)
     {
-        return channelId!=null && channelId.startsWith("/service/");
+        return channelId != null && channelId.startsWith("/service/");
     }
 }
---
+++ cometd-java/cometd-java-common/src/test/java/org/cometd/common/ChannelIdTest.java
@@ -0,0 +1,187 @@
+package org.cometd.common;
+
+import org.cometd.bayeux.ChannelId;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ChannelIdTest
+{
+    @Test
+    public void testDepth()
+    {
+        Assert.assertEquals(1, new ChannelId("/foo").depth());
+        Assert.assertEquals(1,new ChannelId("/foo/").depth());
+        Assert.assertEquals(2,new ChannelId("/foo/bar").depth());
+        Assert.assertEquals(2,new ChannelId("/foo/bar/").depth());
+        Assert.assertEquals(3,new ChannelId("/foo/bar/*").depth());
+        Assert.assertEquals(3,new ChannelId("/foo/bar/**").depth());
+    }
+
+    @Test
+    public void testSegments()
+    {
+        ChannelId channel = new ChannelId("/foo/bar");
+
+        Assert.assertEquals("foo",channel.getSegment(0));
+        Assert.assertEquals("bar",channel.getSegment(1));
+
+        Assert.assertNull(channel.getSegment(2));
+        Assert.assertNull(channel.getSegment(3));
+    }
+
+    @Test
+    public void testIsXxx()
+    {
+        ChannelId id;
+
+        id = new ChannelId("/foo/bar");
+        Assert.assertFalse(id.isDeepWild());
+        Assert.assertFalse(id.isMeta());
+        Assert.assertFalse(id.isService());
+        Assert.assertFalse(id.isWild());
+
+        id = new ChannelId("/foo/*");
+        Assert.assertTrue(id.isShallowWild());
+        Assert.assertFalse(id.isDeepWild());
+        Assert.assertFalse(id.isMeta());
+        Assert.assertFalse(id.isService());
+        Assert.assertTrue(id.isWild());
+
+        id = new ChannelId("/foo/**");
+        Assert.assertTrue(id.isDeepWild());
+        Assert.assertFalse(id.isMeta());
+        Assert.assertFalse(id.isService());
+        Assert.assertTrue(id.isWild());
+
+        id = new ChannelId("/meta/bar");
+        Assert.assertFalse(id.isDeepWild());
+        Assert.assertTrue(id.isMeta());
+        Assert.assertFalse(id.isService());
+        Assert.assertFalse(id.isWild());
+
+        id = new ChannelId("/service/bar");
+        Assert.assertFalse(id.isDeepWild());
+        Assert.assertFalse(id.isMeta());
+        Assert.assertTrue(id.isService());
+        Assert.assertFalse(id.isWild());
+
+        id = new ChannelId("/service/**");
+        Assert.assertTrue(id.isDeepWild());
+        Assert.assertFalse(id.isMeta());
+        Assert.assertTrue(id.isService());
+        Assert.assertTrue(id.isWild());
+    }
+
+    @Test
+    public void testStaticIsXxx()
+    {
+        Assert.assertTrue(ChannelId.isMeta("/meta/bar"));
+        Assert.assertFalse(ChannelId.isMeta("/foo/bar"));
+        Assert.assertTrue(ChannelId.isService("/service/bar"));
+        Assert.assertFalse(ChannelId.isService("/foo/bar"));
+        Assert.assertFalse(ChannelId.isMeta("/"));
+        Assert.assertFalse(ChannelId.isService("/"));
+    }
+
+    @Test
+    public void testIsParent()
+    {
+        ChannelId foo = new ChannelId("/foo");
+        ChannelId bar = new ChannelId("/bar");
+        ChannelId foobar = new ChannelId("/foo/bar");
+        ChannelId foobarbaz = new ChannelId("/foo/bar/baz");
+
+        Assert.assertFalse(foo.isParentOf(foo));
+        Assert.assertTrue(foo.isParentOf(foobar));
+        Assert.assertFalse(foo.isParentOf(foobarbaz));
+
+        Assert.assertFalse(foobar.isParentOf(foo));
+        Assert.assertFalse(foobar.isParentOf(foobar));
+        Assert.assertTrue(foobar.isParentOf(foobarbaz));
+
+        Assert.assertFalse(bar.isParentOf(foo));
+        Assert.assertFalse(bar.isParentOf(foobar));
+        Assert.assertFalse(bar.isParentOf(foobarbaz));
+
+        Assert.assertFalse(foo.isAncestorOf(foo));
+        Assert.assertTrue(foo.isAncestorOf(foobar));
+        Assert.assertTrue(foo.isAncestorOf(foobarbaz));
+
+        Assert.assertFalse(foobar.isAncestorOf(foo));
+        Assert.assertFalse(foobar.isAncestorOf(foobar));
+        Assert.assertTrue(foobar.isAncestorOf(foobarbaz));
+
+        Assert.assertFalse(bar.isAncestorOf(foo));
+        Assert.assertFalse(bar.isAncestorOf(foobar));
+        Assert.assertFalse(bar.isAncestorOf(foobarbaz));
+
+    }
+
+    @Test
+    public void testEquals()
+    {
+        ChannelId foobar0 = new ChannelId("/foo/bar");
+        ChannelId foobar1 = new ChannelId("/foo/bar/");
+        ChannelId foo = new ChannelId("/foo");
+        ChannelId wild = new ChannelId("/foo/*");
+        ChannelId deep = new ChannelId("/foo/**");
+
+        Assert.assertTrue(foobar0.equals(foobar0));
+        Assert.assertTrue(foobar0.equals(foobar1));
+
+        Assert.assertFalse(foobar0.equals(foo));
+        Assert.assertFalse(foobar0.equals(wild));
+        Assert.assertFalse(foobar0.equals(deep));
+    }
+
+    @Test
+    public void testMatches()
+    {
+        ChannelId foobar0 = new ChannelId("/foo/bar");
+        ChannelId foobar1 = new ChannelId("/foo/bar");
+        ChannelId foobarbaz = new ChannelId("/foo/bar/baz");
+        ChannelId foo = new ChannelId("/foo");
+        ChannelId wild = new ChannelId("/foo/*");
+        ChannelId deep = new ChannelId("/foo/**");
+
+        Assert.assertTrue(foobar0.matches(foobar0));
+        Assert.assertTrue(foobar0.matches(foobar1));
+
+        Assert.assertFalse(foo.matches(foobar0));
+        Assert.assertTrue(wild.matches(foobar0));
+        Assert.assertTrue(deep.matches(foobar0));
+
+        Assert.assertFalse(foo.matches(foobarbaz));
+        Assert.assertFalse(wild.matches(foobarbaz));
+        Assert.assertTrue(deep.matches(foobarbaz));
+
+    }
+
+    @Test
+    public void testWilds()
+    {
+        ChannelId id;
+
+        id=new ChannelId("/foo/bar/*");
+        Assert.assertEquals(0,id.getWilds().size());
+
+        id=new ChannelId("/foo");
+        Assert.assertEquals(2,id.getWilds().size());
+        Assert.assertEquals("/*",id.getWilds().get(0));
+        Assert.assertEquals("/**",id.getWilds().get(1));
+
+        id=new ChannelId("/foo/bar");
+        Assert.assertEquals(3,id.getWilds().size());
+        Assert.assertEquals("/foo/*",id.getWilds().get(0));
+        Assert.assertEquals("/foo/**",id.getWilds().get(1));
+        Assert.assertEquals("/**",id.getWilds().get(2));
+
+        id=new ChannelId("/foo/bar/bob");
+        Assert.assertEquals(4,id.getWilds().size());
+        Assert.assertEquals("/foo/bar/*",id.getWilds().get(0));
+        Assert.assertEquals("/foo/bar/**",id.getWilds().get(1));
+        Assert.assertEquals("/foo/**",id.getWilds().get(2));
+        Assert.assertEquals("/**",id.getWilds().get(3));
+
+    }
+}
