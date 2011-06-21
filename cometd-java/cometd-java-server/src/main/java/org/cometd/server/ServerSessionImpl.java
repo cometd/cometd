cometd/cometd

commit a62d72149780ff3ef6f7006add387a1c9cceb480

Fixes COMETD-171 (Remove the asMutable method from the immutable Message API, so it cannot be wrongly used), COMETD-165 (DAGs), and improves on COMETD-144 (Changes made to a Message in Extension rcv() are discarded). ImmutableHashMap has been removed and replaced with normal HashMaps, and the implementation of ServerMessageImpl now leverages HashMapMessage.

cometd-java/cometd-java-server/src/main/java/org/cometd/server/ServerSessionImpl.java
@@ -192,12 +192,18 @@ public class ServerSessionImpl implements ServerSession
     }
 
     /* ------------------------------------------------------------ */
-    protected void doDeliver(ServerSession from, ServerMessage message)
+    protected void doDeliver(ServerSession from, ServerMessage.Mutable mutable)
     {
-        if (message.isMeta())
-            extendSendMeta((message instanceof ServerMessage.Mutable)?(ServerMessage.Mutable)message:((ServerMessageImpl)message).asMutable());
+        ServerMessage message = null;
+        if (mutable.isMeta())
+        {
+            if (!extendSendMeta(mutable))
+                return;
+        }
         else
-            message=extendSendMessage(message);
+        {
+            message = extendSendMessage(mutable);
+        }
         
         if (message==null)
             return;

cometd-java/bayeux-api/src/main/java/org/cometd/bayeux/server/ServerMessage.java
@@ -22,11 +22,6 @@ public interface ServerMessage extends Message
     boolean isLazy();
 
     /**
-     * @return a {@link Mutable} version of this message
-     */
-    //ServerMessage.Mutable asMutable();
-
-    /**
      * The mutable version of a {@link ServerMessage}
      */
     public interface Mutable extends ServerMessage, Message.Mutable
@@ -42,10 +37,5 @@ public interface ServerMessage extends Message
          * @param lazy whether the message is lazy
          */
         void setLazy(boolean lazy);
-
-        /**
-         * @return an {@link ServerMessage immutable} version of this message
-         */
-        ServerMessage asImmutable();
     }
 }

cometd-java/cometd-java-annotations/src/test/java/org/cometd/java/annotation/ServerAnnotationProcessorTest.java
@@ -241,6 +241,8 @@ public class ServerAnnotationProcessorTest
         // Fake another publish
         sessionRef.set(null);
         messageRef.set(null);
+        message = bayeuxServer.newMessage();
+        message.setChannel(channel.getId());
         bayeuxServer.handle((ServerSessionImpl)remote.getServerSession(), message);
 
         assertNull(sessionRef.get());
@@ -370,6 +372,9 @@ public class ServerAnnotationProcessorTest
 
         // Fake another publish
         messageRef.set(null);
+        message = bayeuxServer.newMessage();
+        message.setChannel("/foo/bar/baz");
+        message.setData(new HashMap());
         bayeuxServer.handle((ServerSessionImpl)remote.getServerSession(), message);
 
         assertNull(messageRef.get());

cometd-java/cometd-java-client/src/main/java/org/cometd/client/transport/ClientTransport.java
@@ -1,15 +1,11 @@
 package org.cometd.client.transport;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.cometd.bayeux.Message;
 import org.cometd.common.AbstractTransport;
 import org.cometd.common.HashMapMessage;
-import org.eclipse.jetty.util.ajax.JSON;
 
 /**
  * @version $Revision: 902 $ $Date$
@@ -53,69 +49,8 @@ public abstract class ClientTransport extends AbstractTransport
     public abstract void send(TransportListener listener, Message.Mutable... messages);
 
     /* ------------------------------------------------------------ */
-    protected List<Message.Mutable> toMessages(String content)
+    protected List<Message.Mutable> parseMessages(String content)
     {
-        Object object = _batchJSON.parse(new JSON.StringSource(content));
-        if (object instanceof Message.Mutable)
-            return Collections.singletonList((Message.Mutable)object);
-        return Arrays.asList((Message.Mutable[])object);
+        return HashMapMessage.parseMessages(content);
     }
-
-    /* ------------------------------------------------------------ */
-    /* ------------------------------------------------------------ */
-    private JSON _json=new JSON()
-    {
-        @Override
-        protected Map newMap()
-        {
-            return new HashMap<String, Object>();
-        }
-
-    };
-
-    /* ------------------------------------------------------------ */
-    /* ------------------------------------------------------------ */
-    private JSON _msgJSON=new JSON()
-    {
-        @Override
-        protected Map newMap()
-        {
-            return new HashMapMessage();
-        }
-
-        @Override
-        protected JSON contextFor(String field)
-        {
-            return _json;
-        }
-    };
-
-    /* ------------------------------------------------------------ */
-    /* ------------------------------------------------------------ */
-    private JSON _batchJSON=new JSON()
-    {
-        @Override
-        protected Map newMap()
-        {
-            return new HashMapMessage();
-        }
-
-        @Override
-        protected Object[] newArray(int size)
-        {
-            return new Message.Mutable[size];
-        }
-
-        @Override
-        protected JSON contextFor(String field)
-        {
-            return _json;
-        }
-
-        @Override
-        protected JSON contextForArray()
-        {
-            return _msgJSON;
-        }
-    };
 }

cometd-java/cometd-java-client/src/main/java/org/cometd/client/transport/LongPollingTransport.java
@@ -248,7 +248,7 @@ public class LongPollingTransport extends HttpClientTransport
                 String content=getResponseContent();
                 if (content!=null && content.length()>0)
                 {
-                    List<Message.Mutable> messages = toMessages(getResponseContent());
+                    List<Message.Mutable> messages = parseMessages(getResponseContent());
                     _listener.onMessages(messages);
                 }
                 else

cometd-java/cometd-java-common/src/main/java/org/cometd/common/HashMapMessage.java
@@ -1,7 +1,10 @@
 package org.cometd.common;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.cometd.bayeux.ChannelId;
@@ -10,8 +13,6 @@ import org.eclipse.jetty.util.ajax.JSON;
 
 public class HashMapMessage extends HashMap<String, Object> implements Message.Mutable, JSON.Generator
 {
-    private static JSON __json = new JSON();
-
     public void addJSON(Appendable buffer)
     {
         try
@@ -26,7 +27,13 @@ public class HashMapMessage extends HashMap<String, Object> implements Message.M
 
     public Map<String, Object> getAdvice()
     {
-        return (Map<String, Object>)get(ADVICE_FIELD);
+        Object advice = get(ADVICE_FIELD);
+        if (advice instanceof JSON.Literal)
+        {
+            advice = jsonParser.parse(advice.toString());
+            put(ADVICE_FIELD, advice);
+        }
+        return (Map<String, Object>)advice;
     }
 
     public String getChannel()
@@ -61,8 +68,8 @@ public class HashMapMessage extends HashMap<String, Object> implements Message.M
 
     public String getJSON()
     {
-        Appendable buf = new StringBuilder(__json.getStringBufferSize());
-        __json.appendMap(buf, this);
+        Appendable buf = new StringBuilder(jsonParser.getStringBufferSize());
+        jsonParser.appendMap(buf, this);
         return buf.toString();
     }
 
@@ -100,7 +107,7 @@ public class HashMapMessage extends HashMap<String, Object> implements Message.M
 
         if (ext instanceof JSON.Literal)
         {
-            ext = __json.fromJSON(ext.toString());
+            ext = jsonParser.fromJSON(ext.toString());
             put(EXT_FIELD, ext);
             return (Map<String, Object>)ext;
         }
@@ -150,4 +157,54 @@ public class HashMapMessage extends HashMap<String, Object> implements Message.M
     {
         put(SUCCESSFUL_FIELD, successful);
     }
+
+    public static List<Mutable> parseMessages(String content)
+    {
+        Object object = messagesParser.parse(new JSON.StringSource(content));
+        if (object instanceof Message.Mutable)
+            return Collections.singletonList((Message.Mutable)object);
+        return Arrays.asList((Message.Mutable[])object);
+}
+
+    protected static JSON jsonParser = new JSON();
+    private static JSON _messageParser = new JSON()
+    {
+        @Override
+        protected Map<String, Object> newMap()
+        {
+            return new HashMapMessage();
+        }
+
+        @Override
+        protected JSON contextFor(String field)
+        {
+            return jsonParser;
+        }
+    };
+    private static JSON messagesParser = new JSON()
+    {
+        @Override
+        protected Map<String, Object> newMap()
+        {
+            return new HashMapMessage();
+        }
+
+        @Override
+        protected Object[] newArray(int size)
+        {
+            return new Message.Mutable[size];
+        }
+
+        @Override
+        protected JSON contextFor(String field)
+        {
+            return jsonParser;
+        }
+
+        @Override
+        protected JSON contextForArray()
+        {
+            return _messageParser;
+        }
+    };
 }

cometd-java/cometd-java-common/src/test/java/org/cometd/util/ImmutableHashMapTest.java
@@ -1,207 +1,0 @@
-package org.cometd.util;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.cometd.util.ImmutableHashMap.MutableEntry;
-import org.junit.Assert;
-import org.junit.Test;
-
-
-public class ImmutableHashMapTest 
-{
-    @Test
-    public void testMap()
-    {
-        ImmutableHashMap<String,Object>.Mutable mutable = new ImmutableHashMap<String,Object>(2).asMutable();
-        
-        Assert.assertTrue(mutable.isEmpty());
-        Assert.assertEquals(0,mutable.size());
-        
-        mutable.put("A","one");
-        MutableEntry<String, Object> bee = mutable.getEntryReference("B");
-        bee.setValue("2");
-        mutable.put("C","3");
-        mutable.put("A","1");
-        mutable.put("D","4");
-       
-        Assert.assertFalse(mutable.isEmpty());
-        Assert.assertEquals(4,mutable.size());
-        Assert.assertEquals("1",mutable.get("A"));
-        Assert.assertEquals("2",mutable.get("B"));
-        Assert.assertEquals("2",bee.getValue());
-        Assert.assertEquals("2",bee.asImmutable().getValue());
-        Assert.assertEquals("3",mutable.get("C"));
-        Assert.assertEquals("4",mutable.get("D"));
-        Assert.assertTrue(mutable.containsKey("D"));
-        Assert.assertFalse(mutable.containsKey("E"));
-        Assert.assertEquals("D",mutable.getEntry("D").getKey()); 
-        Assert.assertEquals("4",mutable.getEntry("D").getValue());  
-        
-        Set<String> temp = new HashSet<String>();
-        for (String key : mutable.keySet())
-            temp.add(key);
-        Assert.assertTrue(temp.contains("A"));
-        Assert.assertTrue(temp.contains("B"));
-        Assert.assertTrue(temp.contains("C"));
-        Assert.assertTrue(temp.contains("D"));
-        
-        temp.clear();
-        for (String key : mutable.asImmutable().keySet())
-            temp.add(key);
-        Assert.assertTrue(temp.contains("A"));
-        Assert.assertTrue(temp.contains("B"));
-        Assert.assertTrue(temp.contains("C"));
-        Assert.assertTrue(temp.contains("D"));
-        
-        temp.clear();
-        for (Object value : mutable.values())
-            temp.add(String.valueOf(value));
-        Assert.assertTrue(temp.contains("1"));
-        Assert.assertTrue(temp.contains("2"));
-        Assert.assertTrue(temp.contains("3"));
-        Assert.assertTrue(temp.contains("4"));
-        
-        temp.clear();
-        for (Object value : mutable.asImmutable().values())
-            temp.add(String.valueOf(value));
-        Assert.assertTrue(temp.contains("1"));
-        Assert.assertTrue(temp.contains("2"));
-        Assert.assertTrue(temp.contains("3"));
-        Assert.assertTrue(temp.contains("4"));
-        
-        boolean[] keys={false,false,false,false};
-        for (String s: mutable.keySet())
-            keys[s.charAt(0)-'A']=true;
-        for (boolean b:keys)
-            Assert.assertTrue(b);
-        
-        mutable.remove("A"); 
-        Assert.assertEquals(3,mutable.size());
-        Assert.assertEquals(null,mutable.get("A"));
-        Assert.assertEquals("2",mutable.get("B"));
-        Assert.assertEquals("3",mutable.get("C"));
-        Assert.assertEquals("4",mutable.get("D"));
-
-        bee.setValue(null);
-        Assert.assertEquals(2,mutable.size());
-        Assert.assertEquals(null,mutable.get("A"));
-        Assert.assertEquals(null,mutable.get("B"));
-        Assert.assertEquals("3",mutable.get("C"));
-        Assert.assertEquals("4",mutable.get("D"));
-
-        Iterator<Map.Entry<String,Object>> iter = mutable.entrySet().iterator();
-        while (iter.hasNext())
-        {
-            Map.Entry<String,Object> e=iter.next();
-            if (e.getKey().equals("D"))
-                iter.remove();
-        }
-            
-        Assert.assertEquals(1,mutable.size());
-        Assert.assertEquals(null,mutable.get("A"));
-        Assert.assertEquals(null,mutable.get("B"));
-        Assert.assertEquals("3",mutable.get("C"));
-        Assert.assertEquals(null,mutable.get("D"));
-        
-        mutable.keySet().clear();
-        Assert.assertTrue(mutable.isEmpty());
-        Assert.assertEquals(0,mutable.size());
-
-        mutable.put("A","1");
-        mutable.put("B","2");
-        mutable.put("C","3");
-        mutable.put("D","4");
-        mutable.put("E","5");
-
-        Assert.assertFalse(mutable.isEmpty());
-        Assert.assertEquals(5,mutable.size());
-        mutable.clear();
-        Assert.assertTrue(mutable.isEmpty());
-        Assert.assertEquals(0,mutable.size());
-
-        mutable.getEntry("D").setValue("four");
-        Assert.assertEquals("four",mutable.getEntry("D").getValue());  
-
-        try
-        {
-            mutable.asImmutable().getEntry("D").setValue("IV");
-            Assert.assertTrue(false);
-        }
-        catch(UnsupportedOperationException e)
-        {
-            Assert.assertTrue(true);
-        }
-    }
-    
-    @Test
-    public void testString()
-    {
-        Map<String,Object> map = new ImmutableHashMap<String,Object>(2).asMutable();
-        
-        map.put("A","1");
-        map.put("B","2");
-        map.put("C","3");
-        map.put("D","4");
-        map.put("E","5");
-        
-        String s=map.toString();
-        Assert.assertTrue(s.contains("A=1"));
-        Assert.assertTrue(s.contains("B=2"));
-        Assert.assertTrue(s.contains("C=3"));
-        Assert.assertTrue(s.contains("D=4"));
-        Assert.assertTrue(s.contains("E=5"));
-    }
-
-    @Test
-    public void testImmutable()
-    {
-        ImmutableHashMap<String,Object>.Mutable map = new ImmutableHashMap<String,Object>(2).asMutable();
-        
-        map.put("A","1");
-        map.put("B","2");
-        map.put("C","3");
-        map.put("D","4");
-        map.put("E","5");
-        
-        ImmutableHashMap<String,Object>.Mutable map2 = new ImmutableHashMap<String,Object>(2).asMutable();
-        map2.put("X","1");
-        map2.put("Y","2");
-        map.put("2",map2.asImmutable());
-        
-        Map<String,Object> immutable = map.asImmutable();
-        
-        try { immutable.put("F","6"); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-        try { immutable.clear(); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-        try { immutable.remove("A"); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-        try { immutable.entrySet().clear(); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-        try { Iterator i= immutable.entrySet().iterator(); i.next(); i.remove(); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-    
-
-        try { ((Map<String,Object>)immutable.get("2")).put("Z","3"); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-    
-    }
-
-    @Test
-    public void testMapOfMaps()
-    {
-        ImmutableHashMap<String,Object> map = new ImmutableHashMap<String,Object>();
-        Map<String,Object> data = new HashMap<String,Object>();
-        try {  map.put("data", data); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-        map.asMutable().put("data", data);
-        data.put("field", "value");
-        Assert.assertEquals("{data={field=value}}", map.toString());
-        
-        Entry<String, Object> dataRef=map.asMutable().getEntry("data");
-        ((Map<String,Object>)dataRef.getValue()).put("field","other");
-        Assert.assertEquals("{data={field=other}}", map.toString());
-        
-        dataRef=map.getEntryReference("data");
-        try {  ((Map<String,Object>)dataRef.getValue()).put("field","nope"); Assert.assertTrue(false); } catch (UnsupportedOperationException e) { Assert.assertTrue(true);}
-        Assert.assertEquals("{data={field=other}}", map.toString());      
-    }
-}

commit 6e4ae17987592b6a7411543200bf1f9e851c6a2f

Commit a62d72149780ff3ef6f7006add387a1c9cceb480 broke processing of meta messages in deliver(), restored.

cometd-java/cometd-java-server/src/main/java/org/cometd/server/ServerSessionImpl.java
@@ -222,8 +222,8 @@ public class ServerSessionImpl implements ServerSession
         ServerMessage message = null;
         if (mutable.isMeta())
         {
-            if (!extendSendMeta(mutable))
-                return;
+            if (extendSendMeta(mutable))
+                message = mutable;
         }
         else
         {