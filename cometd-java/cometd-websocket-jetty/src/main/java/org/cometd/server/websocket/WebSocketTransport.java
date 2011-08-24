cd264f4327ad0b9c22f1062188d23b5ffd499423
Simone Bordet <sbordet@intalio.com>
2011-08-24 10:35:34 +0200

    Cleaned up warning message.

--- cometd-java/cometd-websocket-jetty/src/main/java/org/cometd/server/websocket/WebSocketTransport.java
+++ cometd-java/cometd-websocket-jetty/src/main/java/org/cometd/server/websocket/WebSocketTransport.java
@@ -20,17 +20,14 @@ import org.cometd.server.BayeuxServerImpl;
 import org.eclipse.jetty.util.log.Log;
 
 /**
- * @deprecated use org.cometd.websocket.server.WebSocketTransport
+ * @deprecated use {@link org.cometd.websocket.server.WebSocketTransport} instead
  */
 @Deprecated
 public class WebSocketTransport extends org.cometd.websocket.server.WebSocketTransport
 {
-    {
-        Log.warn("Deprecated org.cometd.server.websocket.WebSocketTransport, use org.cometd.websocket.server.WebSocketTransport");
-    }
-
     public WebSocketTransport(BayeuxServerImpl bayeux)
     {
         super(bayeux);
+        Log.warn("Deprecated {}, use {} instead", WebSocketTransport.class.getName(), org.cometd.websocket.server.WebSocketTransport.class.getName());
     }
 }
