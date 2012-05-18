eeccc25321d583f35e514fc1b09188c55ccf04a6
Simone Bordet <simone.bordet@gmail.com>
2012-05-18 13:55:16 +0200

    Addendum fix for COMETD-350 (Reload extension creating two cookies with different path): now reload extension clears the cookie it reads, not the cookie at the cookiePath.

--- cometd-javascript/common/src/main/webapp/org/cometd/ReloadExtension.js
+++ cometd-javascript/common/src/main/webapp/org/cometd/ReloadExtension.js
@@ -60,7 +60,7 @@
                 if (_state && _state.handshakeResponse !== null)
                 {
                     _configure(config);
-
+                    _state.cookiePath = _cookiePath;
                     var cookie = org_cometd.JSON.toJSON(_state);
                     _debug('Reload extension saving cookie value', cookie);
                     org_cometd.COOKIE.set(_cookieName, cookie, {
@@ -131,16 +131,16 @@
                     {
                         try
                         {
+                            var oldState = org_cometd.JSON.fromJSON(cookie);
+
                             // Remove the cookie, not needed anymore
                             org_cometd.COOKIE.set(_cookieName, '', {
                                 'max-age': -1,
-                                path: _cookiePath,
+                                path: oldState.cookiePath,
                                 expires: -1
                             });
 
-                            var oldState = org_cometd.JSON.fromJSON(cookie);
-
-                            if (oldState && oldState.handshakeResponse && _similarState(oldState))
+                            if (oldState.handshakeResponse && _similarState(oldState))
                             {
                                 _debug('Reload extension restoring state', oldState);
                                 setTimeout(function()
