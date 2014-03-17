6f89bddc9b39291901e86ac3eaca4ae8594ef8c0
Simone Bordet <simone.bordet@gmail.com>
2014-03-17 21:19:41 +0100

    Fixed test to be more reliable.

--- cometd-java/cometd-websocket-jetty/src/test/java/org/cometd/websocket/server/BatchedRepliesWebSocketTest.java
+++ cometd-java/cometd-websocket-jetty/src/test/java/org/cometd/websocket/server/BatchedRepliesWebSocketTest.java
@@ -37,6 +37,7 @@ public class BatchedRepliesWebSocketTest extends ClientServerWebSocketTest
         runServer(null);
 
         final AtomicReference<List<Message.Mutable>> batch = new AtomicReference<List<Message.Mutable>>();
+        final CountDownLatch repliesLatch = new CountDownLatch(1);
         ClientTransport transport = new org.cometd.websocket.client.WebSocketTransport(null, wsFactory, null)
         {
             @Override
@@ -47,9 +48,12 @@ public class BatchedRepliesWebSocketTest extends ClientServerWebSocketTest
                     @Override
                     protected void onMessages(List<Message.Mutable> messages)
                     {
-                        if (messages.size() > 1)
-                            batch.set(messages);
                         super.onMessages(messages);
+                        if (messages.size() > 1)
+                        {
+                            batch.set(messages);
+                            repliesLatch.countDown();
+                        }
                     }
                 };
             }
@@ -79,6 +83,7 @@ public class BatchedRepliesWebSocketTest extends ClientServerWebSocketTest
         });
 
         Assert.assertTrue(messageLatch.await(5, TimeUnit.SECONDS));
+        Assert.assertTrue(repliesLatch.await(5, TimeUnit.SECONDS));
         List<Message.Mutable> messages = batch.get();
         Assert.assertNotNull(messages);
         // List must contain subscribe reply and message reply
