6d88455a957b2a0b949d3e990d4253f2fa4db8ec
Simone Bordet <simone.bordet@gmail.com>
2014-09-15 11:38:18 +0200

    Added Spring annotation configuration test.

---
+++ cometd-java/cometd-java-oort/src/test/java/org/cometd/oort/spring/OortConfigurator.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2008-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cometd.oort.spring;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
+import org.cometd.annotation.ServerAnnotationProcessor;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.oort.Oort;
+import org.cometd.oort.Seti;
+import org.cometd.server.BayeuxServerImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.ServletContextAware;
+
+@Configuration
+public class OortConfigurator implements DestructionAwareBeanPostProcessor, ServletContextAware
+{
+    private ServletContext servletContext;
+    private ServerAnnotationProcessor processor;
+
+    @PostConstruct
+    private void init()
+    {
+        this.processor = new ServerAnnotationProcessor(bayeuxServer());
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String name) throws BeansException
+    {
+        processor.processDependencies(bean);
+        processor.processConfigurations(bean);
+        processor.processCallbacks(bean);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String name) throws BeansException
+    {
+        return bean;
+    }
+
+    @Override
+    public void postProcessBeforeDestruction(Object bean, String name) throws BeansException
+    {
+        processor.deprocessCallbacks(bean);
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext)
+    {
+        this.servletContext = servletContext;
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public BayeuxServer bayeuxServer()
+    {
+        BayeuxServerImpl bayeuxServer = new BayeuxServerImpl();
+        bayeuxServer.setOption(ServletContext.class.getName(), servletContext);
+        bayeuxServer.setOption("ws.cometdURLMapping", "/cometd/*");
+        servletContext.setAttribute(BayeuxServer.ATTRIBUTE, bayeuxServer);
+        return bayeuxServer;
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Oort oort()
+    {
+        Oort oort = new Oort(bayeuxServer(), "http://localhost:8080/cometd");
+        servletContext.setAttribute(Oort.OORT_ATTRIBUTE, oort);
+        return oort;
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Seti seti()
+    {
+        Seti seti = new Seti(oort());
+        servletContext.setAttribute(Seti.SETI_ATTRIBUTE, seti);
+        return seti;
+    }
+}
---
+++ cometd-java/cometd-java-oort/src/test/java/org/cometd/oort/spring/OortSpringAnnotationTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2008-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cometd.oort.spring;
+
+import java.beans.Introspector;
+import java.util.Arrays;
+
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.oort.Oort;
+import org.cometd.oort.Seti;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class OortSpringAnnotationTest
+{
+    @Test
+    public void testSpringWiringOfOort() throws Exception
+    {
+        Server server = new Server();
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        WebSocketServerContainerInitializer.configureContext(context);
+        context.addEventListener(new ContextLoaderListener());
+        context.getInitParams().put(ContextLoader.CONFIG_LOCATION_PARAM, "classpath:/applicationContext.xml");
+        server.start();
+
+        ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(context.getServletContext());
+
+        String beanName = Introspector.decapitalize(OortService.class.getSimpleName());
+
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        assertTrue(Arrays.asList(beanNames).contains(beanName));
+
+        OortService service = (OortService)applicationContext.getBean(beanName);
+        assertNotNull(service);
+        Seti seti = service.seti;
+        assertNotNull(seti);
+        Oort oort = seti.getOort();
+        assertNotNull(oort);
+        BayeuxServer bayeux = oort.getBayeuxServer();
+        assertNotNull(bayeux);
+
+        server.stop();
+    }
+}
