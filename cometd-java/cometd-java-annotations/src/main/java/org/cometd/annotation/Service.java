a3832315df2e44cb5b16f016088f17c9b8e697d7
Simone Bordet <simone.bordet@gmail.com>
2015-12-03 11:46:02 +0100

    Issue #351 (Integration with CDI).
    Preparation work for CDI integration.
    @Service has been annotated with @Qualifier and its @Target extended
    so that it is possible to use @Service with CDI.

--- cometd-java/cometd-java-annotations/src/main/java/org/cometd/annotation/Service.java
+++ cometd-java/cometd-java-annotations/src/main/java/org/cometd/annotation/Service.java
@@ -15,7 +15,14 @@
  */
 package org.cometd.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
 
 /**
  * <p>Identifies classes whose instances are services that interact with the Bayeux API.</p>
@@ -25,7 +32,15 @@ import java.lang.annotation.*;
  * <p>Services can have an optional name that is used as a prefix for the {@link org.cometd.bayeux.Session#getId() session identifier},
  * thus helping in debug and logging.</p>
  */
-@Target(ElementType.TYPE)
+@Qualifier
+@Target({
+        ElementType.TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.METHOD,
+        ElementType.FIELD,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.PARAMETER
+})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
