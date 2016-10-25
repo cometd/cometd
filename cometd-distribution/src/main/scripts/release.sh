202d5aebaab473456fee8d7f7614c3f98ffb7e8e
Simone Bordet <simone.bordet@gmail.com>
2016-10-25 11:00:10 +0200

    Removed interactivity.

--- cometd-distribution/src/main/scripts/release.sh
+++ cometd-distribution/src/main/scripts/release.sh
@@ -1,35 +1,24 @@
 #!/usr/bin/env bash
 
-yes_no() {
-    while true; do
-        read -p "$1 " yn
-        case ${yn:-$2} in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-}
-
-set -x
+echo "Running release script with arguments" "$@"
 
 COMETD_DIR=$1
-COMETD_JS_DIR=${COMETD_DIR}/target/release/cometd-javascript
 VERSION=$2
 
-if yes_no "Update JavaScript Resources to NPM/Bower repository ? (Y/n)" y; then
-    git clone git@github.com:cometd/cometd-javascript.git ${COMETD_JS_DIR}
+echo "Updating cometd-javascript repository"
+COMETD_JS_DIR=${COMETD_DIR}/target/release/cometd-javascript
+git clone git@github.com:cometd/cometd-javascript.git ${COMETD_JS_DIR}
 
-    COMETD_JS_SOURCE=${COMETD_DIR}/cometd-javascript/common/target/cometd-javascript-common-${VERSION}
-    cp ${COMETD_JS_SOURCE}/org/cometd.js ${COMETD_JS_DIR}
-    cp ${COMETD_JS_SOURCE}/org/cometd/AckExtension.js ${COMETD_JS_DIR}
-    cp ${COMETD_JS_SOURCE}/org/cometd/ReloadExtension.js ${COMETD_JS_DIR}
-    cp ${COMETD_JS_SOURCE}/org/cometd/TimeStampExtension.js ${COMETD_JS_DIR}
-    cp ${COMETD_JS_SOURCE}/org/cometd/TimeSyncExtension.js ${COMETD_JS_DIR}
+COMETD_JS_SOURCE=${COMETD_DIR}/cometd-javascript/common/target/cometd-javascript-common-${VERSION}
+cp ${COMETD_JS_SOURCE}/org/cometd.js ${COMETD_JS_DIR}
+cp ${COMETD_JS_SOURCE}/org/cometd/AckExtension.js ${COMETD_JS_DIR}
+cp ${COMETD_JS_SOURCE}/org/cometd/ReloadExtension.js ${COMETD_JS_DIR}
+cp ${COMETD_JS_SOURCE}/org/cometd/TimeStampExtension.js ${COMETD_JS_DIR}
+cp ${COMETD_JS_SOURCE}/org/cometd/TimeSyncExtension.js ${COMETD_JS_DIR}
 
-    cd ${COMETD_JS_DIR}
+cd ${COMETD_JS_DIR}
 
-    cat <<EOF > ${COMETD_JS_DIR}/package.json
+cat <<EOF > ${COMETD_JS_DIR}/package.json
 {
   "name": "cometd",
   "version": "${VERSION}",
@@ -48,26 +37,25 @@ if yes_no "Update JavaScript Resources to NPM/Bower repository ? (Y/n)" y; then
 }
 EOF
 
-    git add .
-    git commit -m "Release ${VERSION}."
-    git tag -am "Release ${VERSION}." ${VERSION}
-    git push --follow-tags
-fi
+git add .
+git commit -m "Release ${VERSION}."
+git tag -am "Release ${VERSION}." ${VERSION}
+git push --follow-tags
 
+echo "Updating cometd-dojo repository"
 COMETD_DOJO_DIR=${COMETD_DIR}/target/release/cometd-dojo
 DOJO_VERSION=$3
 
-if yes_no "Update JavaScript Resources to Dojo repository ? (Y/n)" y; then
-    git clone git@github.com:cometd/cometd-dojo.git ${COMETD_DOJO_DIR}
+git clone git@github.com:cometd/cometd-dojo.git ${COMETD_DOJO_DIR}
 
-    COMETD_DOJO_SOURCE=${COMETD_DIR}/cometd-javascript/dojo/target/cometd-javascript-dojo-${VERSION}
-    cp -r ${COMETD_DOJO_SOURCE}/org/* ${COMETD_DOJO_DIR}/org
-    cp ${COMETD_DOJO_SOURCE}/dojox/cometd.js ${COMETD_DOJO_DIR}
-    cp -r ${COMETD_DOJO_SOURCE}/dojox/cometd/* ${COMETD_DOJO_DIR}/cometd
+COMETD_DOJO_SOURCE=${COMETD_DIR}/cometd-javascript/dojo/target/cometd-javascript-dojo-${VERSION}
+cp -r ${COMETD_DOJO_SOURCE}/org/* ${COMETD_DOJO_DIR}/org
+cp ${COMETD_DOJO_SOURCE}/dojox/cometd.js ${COMETD_DOJO_DIR}
+cp -r ${COMETD_DOJO_SOURCE}/dojox/cometd/* ${COMETD_DOJO_DIR}/cometd
 
-    cd ${COMETD_DOJO_DIR}
+cd ${COMETD_DOJO_DIR}
 
-    cat <<EOF > ${COMETD_DOJO_DIR}/package.json
+cat <<EOF > ${COMETD_DOJO_DIR}/package.json
 {
   "name": "CometD",
   "version": "${VERSION}",
@@ -88,5 +76,3 @@ if yes_no "Update JavaScript Resources to Dojo repository ? (Y/n)" y; then
   }
 }
 EOF
-
-fi
