ab9c5c80dd3de1d356a8d8ab6238debec8ac5b89
Simone Bordet <simone.bordet@gmail.com>
2016-10-25 10:48:36 +0200

    Added sync for the cometd-dojo repository.

--- cometd-distribution/src/main/scripts/release.sh
+++ cometd-distribution/src/main/scripts/release.sh
@@ -33,7 +33,18 @@ if yes_no "Update JavaScript Resources to NPM/Bower repository ? (Y/n)" y; then
 {
   "name": "cometd",
   "version": "${VERSION}",
-  "main": "cometd.js"
+  "description": "Comet and WebSocket library for web messaging",
+  "keywords": ["comet", "websocket", "messaging", "pubsub", "publish", "subscribe", "rpc", "p2p", "peer-to-peer"],
+  "homepage": "https://cometd.org",
+  "bugs": {
+    "url": "https://bugs.cometd.org"
+  },
+  "license": "Apache-2.0",
+  "main": "cometd.js",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/cometd/cometd.git"
+  }
 }
 EOF
 
@@ -41,4 +52,41 @@ EOF
     git commit -m "Release ${VERSION}."
     git tag -am "Release ${VERSION}." ${VERSION}
     git push --follow-tags
+fi
+
+COMETD_DOJO_DIR=${COMETD_DIR}/target/release/cometd-dojo
+DOJO_VERSION=$3
+
+if yes_no "Update JavaScript Resources to Dojo repository ? (Y/n)" y; then
+    git clone git@github.com:cometd/cometd-dojo.git ${COMETD_DOJO_DIR}
+
+    COMETD_DOJO_SOURCE=${COMETD_DIR}/cometd-javascript/dojo/target/cometd-javascript-dojo-${VERSION}
+    cp -r ${COMETD_DOJO_SOURCE}/org/* ${COMETD_DOJO_DIR}/org
+    cp ${COMETD_DOJO_SOURCE}/dojox/cometd.js ${COMETD_DOJO_DIR}
+    cp -r ${COMETD_DOJO_SOURCE}/dojox/cometd/* ${COMETD_DOJO_DIR}/cometd
+
+    cd ${COMETD_DOJO_DIR}
+
+    cat <<EOF > ${COMETD_DOJO_DIR}/package.json
+{
+  "name": "CometD",
+  "version": "${VERSION}",
+  "description": "Comet and WebSocket library for web messaging",
+  "main": "cometd.js",
+  "licenses": [{
+    "type":"Apache License, Version 2.0",
+    "url":"http://www.apache.org/licenses/LICENSE-2.0"
+  }],
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/cometd/cometd-dojo.git"
+  },
+  "bugs": "https://bugs.cometd.org",
+  "homepage": "https://cometd.org",
+  "dependencies": {
+    "dojo": "${DOJO_VERSION}"
+  }
+}
+EOF
+
 fi
