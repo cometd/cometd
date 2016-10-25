3039d0515956cfe8c52eb9421a1f209b2d587f5c
Simone Bordet <simone.bordet@gmail.com>
2016-10-25 11:41:15 +0200

    Copying files in verbose mode.

--- cometd-distribution/src/main/scripts/release.sh
+++ cometd-distribution/src/main/scripts/release.sh
@@ -19,11 +19,11 @@ COMETD_JS_DIR=${COMETD_DIR}/target/release/cometd-javascript
 git clone git@github.com:cometd/cometd-javascript.git ${COMETD_JS_DIR}
 
 COMETD_JS_SOURCE=${COMETD_DIR}/cometd-javascript/common/target/cometd-javascript-common-${VERSION}
-cp ${COMETD_JS_SOURCE}/org/cometd.js ${COMETD_JS_DIR}
-cp ${COMETD_JS_SOURCE}/org/cometd/AckExtension.js ${COMETD_JS_DIR}
-cp ${COMETD_JS_SOURCE}/org/cometd/ReloadExtension.js ${COMETD_JS_DIR}
-cp ${COMETD_JS_SOURCE}/org/cometd/TimeStampExtension.js ${COMETD_JS_DIR}
-cp ${COMETD_JS_SOURCE}/org/cometd/TimeSyncExtension.js ${COMETD_JS_DIR}
+cp -v ${COMETD_JS_SOURCE}/org/cometd.js ${COMETD_JS_DIR}
+cp -v ${COMETD_JS_SOURCE}/org/cometd/AckExtension.js ${COMETD_JS_DIR}
+cp -v ${COMETD_JS_SOURCE}/org/cometd/ReloadExtension.js ${COMETD_JS_DIR}
+cp -v ${COMETD_JS_SOURCE}/org/cometd/TimeStampExtension.js ${COMETD_JS_DIR}
+cp -v ${COMETD_JS_SOURCE}/org/cometd/TimeSyncExtension.js ${COMETD_JS_DIR}
 
 cd ${COMETD_JS_DIR}
 
@@ -58,9 +58,9 @@ DOJO_VERSION=$3
 git clone git@github.com:cometd/cometd-dojo.git ${COMETD_DOJO_DIR}
 
 COMETD_DOJO_SOURCE=${COMETD_DIR}/cometd-javascript/dojo/target/cometd-javascript-dojo-${VERSION}
-cp -r ${COMETD_DOJO_SOURCE}/org/* ${COMETD_DOJO_DIR}/org
-cp ${COMETD_DOJO_SOURCE}/dojox/cometd.js ${COMETD_DOJO_DIR}
-cp -r ${COMETD_DOJO_SOURCE}/dojox/cometd/* ${COMETD_DOJO_DIR}/cometd
+cp -r -v ${COMETD_DOJO_SOURCE}/org/* ${COMETD_DOJO_DIR}/org
+cp -v ${COMETD_DOJO_SOURCE}/dojox/cometd.js ${COMETD_DOJO_DIR}
+cp -r -v ${COMETD_DOJO_SOURCE}/dojox/cometd/* ${COMETD_DOJO_DIR}/cometd
 
 cd ${COMETD_DOJO_DIR}
 
