ac113799bcf52c10d42b9e88c8836e971146fca2
Greg Wilkins <gregw@webtide.com>
2018-01-21 16:16:26 +0100

    Issue #762 consistent JSONDataFilter
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- cometd-java/cometd-java-server/src/main/java/org/cometd/server/filter/JSONDataFilter.java
+++ cometd-java/cometd-java-server/src/main/java/org/cometd/server/filter/JSONDataFilter.java
@@ -16,9 +16,14 @@
 package org.cometd.server.filter;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.cometd.bayeux.server.ServerChannel;
 import org.cometd.bayeux.server.ServerSession;
@@ -41,12 +46,11 @@ public class JSONDataFilter implements DataFilter {
         if (data == null) {
             return null;
         }
-
         if (data instanceof Map) {
             return filterMap(from, to, (Map)data);
         }
         if (data instanceof List) {
-            return filterArray(from, to, ((List)data).toArray());
+            return filterList(from, to, (List)data);
         }
         if (data instanceof Collection) {
             return filterArray(from, to, ((Collection)data).toArray());
@@ -83,29 +87,87 @@ public class JSONDataFilter implements DataFilter {
             return null;
         }
 
-        int length = Array.getLength(array);
-
-        for (int i = 0; i < length; i++) {
-            Array.set(array, i, filter(from, to, Array.get(array, i)));
+        Object[] mutated = null;        
+        for (int i=Array.getLength(array); i-->0;) {
+            Object original = Array.get(array,i);
+            Object filtered = filter(from, to , original);
+            if (original!=filtered) {
+                if (mutated==null)
+                    mutated=Arrays.copyOf((Object[])array,Array.getLength(array));
+                mutated[i] = filtered;
+            }
         }
 
-        return array;
+        return mutated==null?array:mutated;
     }
 
-    protected Object filterMap(ServerSession from, ServerChannel to, Map<String, Object> map) {
+    protected Object filterList(ServerSession from, ServerChannel to, List<Object> list) {
+        if (list == null) {
+            return null;
+        }
+
+        List<Object> mutated = null;
+        for (int i=list.size(); i-->0;) {
+            Object original = list.get(i);
+            Object filtered = filter(from, to ,original);
+            if (original!=filtered) {
+                if (mutated==null)
+                    mutated = new ArrayList<>(list);
+                mutated.set(i,filtered);
+            }
+        }
+            
+        return mutated==null?list:mutated;
+    }
+    
+    protected Object filterCollection(ServerSession from, ServerChannel to, Collection<Object> collection) {
+        if (collection == null) {
+            return null;
+        }
+
+        List<Object> mutated = null;
+        for (Iterator<Object> i = collection.iterator(); i.hasNext();) {
+            Object original = i.next();
+            Object filtered = filter(from, to ,original);
+            if (original!=filtered) {
+                if (mutated==null) {
+                    mutated = new ArrayList<>(collection.size());
+                    for (Object copy : collection) {
+                        if (copy==original)
+                            break;
+                        mutated.add(copy);
+                    }
+                }
+                
+                mutated.add(filtered);
+            }
+            else if (mutated!=null) {
+                mutated.add(original);
+            }
+        }
+         
+        return mutated==null?collection:mutated;
+    }
+
+    
+        
+    protected Object filterMap(ServerSession from, ServerChannel to, Map<Object, Object> map) {
         if (map == null) {
             return null;
         }
 
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
+        Map<Object, Object> mutated = null;
+        for (Entry<Object, Object> entry : map.entrySet()) {
             Object original = entry.getValue();
             Object filtered = filter(from, to ,original);
             if (original!=filtered) {
-                entry.setValue(filtered);
+                if (mutated==null)
+                    mutated = new HashMap<>(map);
+                mutated.put(entry.getKey(),filtered);
             }
         }
-
-        return map;
+            
+        return mutated==null?map:mutated;
     }
 
     protected Object filterObject(ServerSession from, ServerChannel to, Object obj) {
