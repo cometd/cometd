95ad8071f2b0fa52f7582639f512953c41d4a572
Simone Bordet <simone.bordet@gmail.com>
2019-01-24 11:08:16 +0100

    Avoid split packages for the OkHttp WebSocket client implementation.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- cometd-java/cometd-java-websocket/cometd-java-websocket-okhttp-client/src/main/java/org/cometd/websocket/client/okhttp/OkHttpWebsocketTransport.java
+++ cometd-java/cometd-java-websocket/cometd-java-websocket-okhttp-client/src/main/java/org/cometd/websocket/client/okhttp/OkHttpWebsocketTransport.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.cometd.websocket.client;
+package org.cometd.websocket.client.okhttp;
 
 import java.io.IOException;
 import java.net.CookieStore;
