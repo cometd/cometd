46e24fc87044d85f1ce93c46f876332e28ee9227
Simone Bordet <simone.bordet@gmail.com>
2020-08-28 13:38:50 +0200

    Documentation improvements.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

---
+++ cometd-documentation/src/main/java/org/cometd/documentation/server/ServerServiceIntegrationDocs.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2008-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cometd.documentation.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.cometd.annotation.server.ServerAnnotationProcessor;
+import org.cometd.bayeux.server.BayeuxServer;
+
+@SuppressWarnings("unused")
+public class ServerServiceIntegrationDocs {
+    static class EchoService {
+        public EchoService(BayeuxServer bayeuxServer) {
+        }
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    // tag::configServlet[]
+    public class ConfigurationServlet extends HttpServlet {
+        @Override
+        public void init() {
+            // Grab the Bayeux object
+            BayeuxServer bayeux = (BayeuxServer)getServletContext().getAttribute(BayeuxServer.ATTRIBUTE);
+            new EchoService(bayeux);
+            // Create other services here
+
+            // This is also the place where you can configure the Bayeux object
+            // by adding extensions or specifying a SecurityPolicy
+        }
+
+        @Override
+        public void service(ServletRequest req, ServletResponse res) throws ServletException {
+            // This is a configuration servlet, requests are not serviced.
+            throw new ServletException();
+        }
+    }
+    // end::configServlet[]
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    // tag::configListener[]
+    public class ConfigurationListener implements ServletContextAttributeListener {
+        @Override
+        public void attributeAdded(ServletContextAttributeEvent event) {
+            if (BayeuxServer.ATTRIBUTE.equals(event.getName())) {
+                // Grab the Bayeux object
+                BayeuxServer bayeux = (BayeuxServer)event.getValue();
+                new EchoService(bayeux);
+                // Create other services here
+
+                // This is also the place where you can configure the Bayeux object
+                // by adding extensions or specifying a SecurityPolicy
+            }
+        }
+
+        @Override
+        public void attributeRemoved(ServletContextAttributeEvent event) {
+        }
+
+        @Override
+        public void attributeReplaced(ServletContextAttributeEvent event) {
+        }
+    }
+    // end::configListener[]
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    // tag::annotationConfigServlet[]
+    public class AnnotationConfigurationServlet extends HttpServlet {
+        private final List<Object> services = new ArrayList<>();
+        private ServerAnnotationProcessor processor;
+
+        @Override
+        public void init() {
+            // Grab the BayeuxServer object
+            BayeuxServer bayeuxServer = (BayeuxServer)getServletContext().getAttribute(BayeuxServer.ATTRIBUTE);
+
+            // Create the annotation processor
+            processor = new ServerAnnotationProcessor(bayeuxServer);
+
+            // Create your annotated service instance and process it
+            Object service = new EchoService(bayeuxServer);
+            processor.process(service);
+            services.add(service);
+
+            // Create other services here
+
+            // This is also the place where you can configure the Bayeux object
+            // by adding extensions or specifying a SecurityPolicy
+        }
+
+        @Override
+        public void destroy() {
+            // Deprocess the services that have been created
+            for (Object service : services) {
+                processor.deprocess(service);
+            }
+            services.clear();
+        }
+
+        @Override
+        public void service(ServletRequest req, ServletResponse res) throws ServletException {
+            // This is a configuration servlet, requests are not serviced.
+            throw new ServletException();
+        }
+    }
+    // end::annotationConfigServlet[]
+
+    static class FooService {
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    // tag::anotherServlet[]
+    public class AnotherServlet extends HttpServlet {
+        @Override
+        protected void service(HttpServletRequest request, HttpServletResponse response) {
+            FooService service = (FooService)getServletContext().getAttribute("com.acme.cometd.FooService");
+            // Use the foo service here
+        }
+    }
+    // end::anotherServlet[]
+}
---
+++ cometd-documentation/src/main/java/org/cometd/documentation/server/ServerServiceIntegrationSpringDocs.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2008-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cometd.documentation.server;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+import org.cometd.annotation.Listener;
+import org.cometd.annotation.Service;
+import org.cometd.annotation.Session;
+import org.cometd.annotation.server.AnnotationCometDServlet;
+import org.cometd.annotation.server.ServerAnnotationProcessor;
+import org.cometd.bayeux.Promise;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ServerMessage;
+import org.cometd.bayeux.server.ServerSession;
+import org.cometd.documentation.server.ServerServiceIntegrationSpringDocs.EchoService;
+import org.cometd.server.AbstractServerTransport;
+import org.cometd.server.BayeuxServerImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.ServletContextAware;
+
+@SuppressWarnings("unused")
+public class ServerServiceIntegrationSpringDocs {
+    @SuppressWarnings("InnerClassMayBeStatic")
+    // tag::service[]
+    @javax.inject.Named // Tells Spring that this is a bean
+    @javax.inject.Singleton // Tells Spring that this is a singleton
+    @Service("echoService")
+    public class EchoService {
+        @Inject
+        private BayeuxServer bayeux;
+        @Session
+        private ServerSession serverSession;
+
+        @PostConstruct
+        public void init() {
+            System.out.println("Echo Service Initialized");
+        }
+
+        @Listener("/echo")
+        public void echo(ServerSession remote, ServerMessage.Mutable message) {
+            String channel = message.getChannel();
+            Object data = message.getData();
+            remote.deliver(serverSession, channel, data, Promise.noop());
+        }
+    }
+    // end::service[]
+
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    // tag::configurer[]
+    @Configuration
+    public class Configurer implements DestructionAwareBeanPostProcessor, ServletContextAware {
+        private BayeuxServer bayeuxServer;
+        private ServerAnnotationProcessor processor;
+
+        @Bean(initMethod = "start", destroyMethod = "stop")
+        public BayeuxServer bayeuxServer() {
+            BayeuxServerImpl bayeuxServer = new BayeuxServerImpl();
+            // Configure BayeuxServer here.
+            bayeuxServer.setOption(AbstractServerTransport.TIMEOUT_OPTION, 15000);
+            return bayeuxServer;
+        }
+
+        @Inject
+        private void setBayeuxServer(BayeuxServer bayeuxServer) {
+            this.bayeuxServer = bayeuxServer;
+        }
+
+        @PostConstruct
+        private void init() {
+            this.processor = new ServerAnnotationProcessor(bayeuxServer);
+        }
+
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String name) throws BeansException {
+            // Intercept bean initialization and process CometD annotations.
+            processor.processDependencies(bean);
+            processor.processConfigurations(bean);
+            processor.processCallbacks(bean);
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String name) throws BeansException {
+            return bean;
+        }
+
+        @Override
+        public boolean requiresDestruction(Object bean) {
+            return true;
+        }
+
+        @Override
+        public void postProcessBeforeDestruction(Object bean, String name) throws BeansException {
+            processor.deprocessCallbacks(bean);
+        }
+
+        @Override
+        public void setServletContext(ServletContext servletContext) {
+            servletContext.setAttribute(BayeuxServer.ATTRIBUTE, bayeuxServer);
+        }
+    }
+    // end::configurer[]
+}
+
+// tag::boot[]
+@SpringBootApplication // <1>
+class CometDApplication implements ServletContextInitializer { // <2>
+    public static void main(String[] args) {
+        SpringApplication.run(CometDApplication.class, args);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        ServletRegistration.Dynamic cometdServlet = servletContext.addServlet("cometd", AnnotationCometDServlet.class); // <3>
+        cometdServlet.addMapping("/cometd/*");
+        cometdServlet.setAsyncSupported(true);
+        cometdServlet.setLoadOnStartup(1);
+        cometdServlet.setInitParameter("services", EchoService.class.getName());
+        // Possible additional CometD Servlet configuration.
+    }
+}
+// end::boot[]
