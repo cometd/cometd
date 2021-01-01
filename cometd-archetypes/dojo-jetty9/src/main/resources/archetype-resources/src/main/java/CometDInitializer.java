e608061dc47e91299e7249cade596788d2716b24
Simone Bordet <simone.bordet@gmail.com>
2021-01-01 16:34:04 +0100

    Restored archetype source prototypes, changed by mistake by the import optimization.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- cometd-archetypes/dojo-jetty9/src/main/resources/archetype-resources/src/main/java/CometDInitializer.java
+++ cometd-archetypes/dojo-jetty9/src/main/resources/archetype-resources/src/main/java/CometDInitializer.java
@@ -1,3 +1,9 @@
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\')
+
+package ${package};
+
 import java.io.IOException;
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
