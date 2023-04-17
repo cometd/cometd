d8d809ca8b334a9bb37370961197002547129818
Ludovic Orban <lorban@bitronix.be>
2023-04-17 11:25:28 +0200

    change license header
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- cometd-java/cometd-java-server/cometd-java-server-servlet/src/main/java/org/cometd/server/servlet/transport/SerializedInvoker.java
+++ cometd-java/cometd-java-server/cometd-java-server-servlet/src/main/java/org/cometd/server/servlet/transport/SerializedInvoker.java
@@ -1,16 +1,18 @@
-//
-// ========================================================================
-// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
-//
-// This program and the accompanying materials are made available under the
-// terms of the Eclipse Public License v. 2.0 which is available at
-// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
-// which is available at https://www.apache.org/licenses/LICENSE-2.0.
-//
-// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
-// ========================================================================
-//
-
+/*
+ * Copyright (c) 2008-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.cometd.server.servlet.transport;
 
 import java.util.concurrent.atomic.AtomicReference;
