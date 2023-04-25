af255e77a84db00a310cb8f51234df96b16c6e72
Ludovic Orban <lorban@bitronix.be>
2023-04-25 15:42:40 +0200

    add more tests
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

---
+++ cometd-java/cometd-java-server/cometd-java-server-handler/src/test/java/org/cometd/server/handler/ServerShutdownTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2008-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cometd.server.handler;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.client.ContentResponse;
+import org.eclipse.jetty.client.FutureResponseListener;
+import org.eclipse.jetty.client.Request;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class ServerShutdownTest extends AbstractBayeuxClientServerTest
+{
+    @ParameterizedTest
+    @MethodSource("transports")
+    public void testServerShutdown(String serverTransport) throws Exception {
+        startServer(serverTransport, null);
+
+        Request handshake = newBayeuxRequest("" +
+                "[{" +
+                "\"channel\": \"/meta/handshake\"," +
+                "\"version\": \"1.0\"," +
+                "\"minimumVersion\": \"1.0\"," +
+                "\"supportedConnectionTypes\": [\"long-polling\"]" +
+                "}]");
+        ContentResponse response = handshake.send();
+        Assertions.assertEquals(200, response.getStatus());
+
+        String clientId = extractClientId(response);
+
+        Request connect = newBayeuxRequest("" +
+                "[{" +
+                "\"channel\": \"/meta/connect\"," +
+                "\"clientId\": \"" + clientId + "\"," +
+                "\"connectionType\": \"long-polling\"," +
+                "\"advice\": { \"timeout\": 0 }" +
+                "}]");
+        response = connect.send();
+
+        Assertions.assertEquals(200, response.getStatus());
+
+        connect = newBayeuxRequest("" +
+                "[{" +
+                "\"channel\": \"/meta/connect\"," +
+                "\"clientId\": \"" + clientId + "\"," +
+                "\"connectionType\": \"long-polling\"" +
+                "}]");
+        FutureResponseListener futureResponse = new FutureResponseListener(connect);
+        connect.send(futureResponse);
+
+        // Wait for the connect to arrive to the server
+        Thread.sleep(1000);
+
+        // Shutdown the server
+        server.stop();
+        server.join();
+
+        // Expect the connect to be back with an exception
+        try {
+            futureResponse.get(timeout * 2, TimeUnit.SECONDS);
+            Assertions.fail();
+        } catch (ExecutionException expected) {
+        }
+    }
+}
