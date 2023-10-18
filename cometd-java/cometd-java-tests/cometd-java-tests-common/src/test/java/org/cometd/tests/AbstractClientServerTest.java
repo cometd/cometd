08900265f8e06a57d825656bc5d450069994663b
Simone Bordet <simone.bordet@gmail.com>
2023-10-18 12:36:15 +0200

    Added test case to show how to intercept the HTTP response.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- cometd-java/cometd-java-tests/cometd-java-tests-common/src/test/java/org/cometd/tests/AbstractClientServerTest.java
+++ cometd-java/cometd-java-tests/cometd-java-tests-common/src/test/java/org/cometd/tests/AbstractClientServerTest.java
@@ -27,6 +27,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import javax.websocket.ContainerProvider;
 import javax.websocket.WebSocketContainer;
+
 import okhttp3.OkHttpClient;
 import org.cometd.bayeux.server.BayeuxServer;
 import org.cometd.client.BayeuxClient;
@@ -71,11 +72,11 @@ public abstract class AbstractClientServerTest {
     protected String cometdServletPath = "/cometd";
     protected String cometdURL;
     protected BayeuxServer bayeux;
-    private ScheduledExecutorService scheduler;
-    private HttpClient httpClient;
-    private WebSocketContainer wsContainer;
-    private WebSocketClient wsClient;
-    private OkHttpClient okHttpClient;
+    protected ScheduledExecutorService scheduler;
+    protected HttpClient httpClient;
+    protected WebSocketContainer wsContainer;
+    protected WebSocketClient wsClient;
+    protected OkHttpClient okHttpClient;
 
     public void start(Transport transport) throws Exception {
         start(transport, serverOptions(transport));
@@ -94,6 +95,7 @@ public abstract class AbstractClientServerTest {
         server.addConnector(connector);
 
         context = new ServletContextHandler(server, "/");
+        configure(context);
 
         switch (transport) {
             case JAVAX_WEBSOCKET:
@@ -125,6 +127,9 @@ public abstract class AbstractClientServerTest {
         cometdURL = "http://localhost:" + port + cometdServletPath;
 
         bayeux = (BayeuxServer)context.getServletContext().getAttribute(BayeuxServer.ATTRIBUTE);
+    }
+
+    protected void configure(ServletContextHandler context) {
     }
 
     protected void startClient(Transport transport) throws Exception {
