a9abeddc231cb6f127e9687ec927aa7c5519edee
Simone Bordet <simone.bordet@gmail.com>
2023-10-31 17:33:08 +0100

    Fixed source path for JavaScript files.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- cometd-distribution/src/main/scripts/release.sh
+++ cometd-distribution/src/main/scripts/release.sh
@@ -41,7 +41,7 @@ echo "Updating cometd-javascript repository"
 COMETD_JS_DIR=${COMETD_DIR}/target/release/cometd-javascript
 git clone --branch 8.0.x git@github.com:cometd/cometd-javascript.git ${COMETD_JS_DIR}
 
-COMETD_JS_SOURCE=${COMETD_DIR}/cometd-javascript/common/target/cometd-javascript-common-${VERSION}/js
+COMETD_JS_SOURCE=${COMETD_DIR}/cometd-javascript/cometd-javascript-common/target/cometd-javascript-common-${VERSION}/js
 cp -v ${COMETD_JS_SOURCE}/cometd/cometd.js ${COMETD_JS_DIR}
 cp -v ${COMETD_JS_SOURCE}/cometd/*Extension.js ${COMETD_JS_DIR}
 cp -v ${COMETD_JS_SOURCE}/cometd/cometd.d.ts ${COMETD_JS_DIR}
