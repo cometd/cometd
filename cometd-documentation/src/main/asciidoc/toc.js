5edb3fa475b93bcafe4a46d0b58cfdcca49705cc
Simone Bordet <simone.bordet@gmail.com>
2024-04-16 16:08:14 +0200

    Updated documentation.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- cometd-documentation/src/main/asciidoc/toc.js
+++ cometd-documentation/src/main/asciidoc/toc.js
@@ -15,46 +15,46 @@
  */
 
 
-document.addEventListener('DOMContentLoaded', () => dynamicTOC());
+document.addEventListener("DOMContentLoaded", () => dynamicTOC());
 
 function dynamicTOC() {
-    const content = document.getElementById('content');
+    const content = document.getElementById("content");
     // Bind a click listener to all section titles.
-    const sectionTitles = content.querySelectorAll('a.link');
+    const sectionTitles = content.querySelectorAll("a.link");
     for (const sectionTitle of sectionTitles) {
-        sectionTitle.addEventListener('click', event => collapseThenExpand(hash(event.target)));
+        sectionTitle.addEventListener("click", event => collapseThenExpand(hash(event.target)));
     }
     // Bind a click listener to all inline links to documentation sections.
-    const inlineLinks = content.querySelectorAll('p > a');
+    const inlineLinks = content.querySelectorAll("p > a");
     for (const inlineLink of inlineLinks) {
-        const linkHash = inlineLink.hash || '';
-        if (linkHash.startsWith('#')) {
-            inlineLink.addEventListener('click', event => collapseThenExpand(hash(event.target)));
+        const linkHash = inlineLink.hash || "";
+        if (linkHash.startsWith("#")) {
+            inlineLink.addEventListener("click", event => collapseThenExpand(hash(event.target)));
         }
     }
 
     // Bind a click listener to all TOC titles.
-    const toc = document.getElementById('toc');
-    const tocTitles = toc.querySelectorAll('a');
+    const toc = document.getElementById("toc");
+    const tocTitles = toc.querySelectorAll("a");
     for (const tocTitle of tocTitles) {
-        tocTitle.addEventListener('click', event => collapseThenExpand(hash(event.target)));
+        tocTitle.addEventListener("click", event => collapseThenExpand(hash(event.target)));
     }
 
     // Add the icons to TOC nodes.
-    const nodes = toc.querySelectorAll('li');
+    const nodes = toc.querySelectorAll("li");
     for (const node of nodes) {
-        const span = document.createElement('span');
+        const span = document.createElement("span");
         const css = span.classList;
-        if (node.querySelector(':scope > ul')) {
-            css.add('toc-toggle');
+        if (node.querySelector(":scope > ul")) {
+            css.add("toc-toggle");
             // Font-Awesome classes.
-            css.add('fa');
-            css.add('fa-caret-right');
-            span.addEventListener('click', event => toggle(event.target));
+            css.add("fa");
+            css.add("fa-caret-right");
+            span.addEventListener("click", event => toggle(event.target));
         } else {
-            css.add('toc-item');
+            css.add("toc-item");
             // The "icon" is the &bull; HTML entity.
-            span.appendChild(document.createTextNode('•'));
+            span.appendChild(document.createTextNode("•"));
         }
         node.prepend(span);
     }
@@ -63,20 +63,20 @@ function dynamicTOC() {
 }
 
 function hash(element) {
-    const a = element.closest('a');
+    const a = element.closest("a");
     return a ? a.hash : null;
 }
 
 function collapseThenExpand(hash) {
-    const toc = document.getElementById('toc');
+    const toc = document.getElementById("toc");
     if (hash) {
-        const current = toc.querySelector('a.toc-current');
+        const current = toc.querySelector("a.toc-current");
         if (current) {
-            current.classList.remove('toc-current');
+            current.classList.remove("toc-current");
         }
-        const anchor = toc.querySelector('a[href="' + hash + '"');
+        const anchor = toc.querySelector(`a[href="${hash}"`);
         if (anchor) {
-            anchor.classList.add('toc-current');
+            anchor.classList.add("toc-current");
             collapse(toc);
             expand(anchor.parentNode);
         }
@@ -86,49 +86,49 @@ function collapseThenExpand(hash) {
 }
 
 function collapse(node) {
-    const sections = node.querySelectorAll('ul');
+    const sections = node.querySelectorAll("ul");
     for (const section of sections) {
         const css = section.classList;
         // Always show first level TOC titles.
-        if (!css.contains('sectlevel1')) {
-            css.add('hidden');
+        if (!css.contains("sectlevel1")) {
+            css.add("hidden");
         }
     }
     // Show the collapsed icon.
-    const spans = node.querySelectorAll('span.toc-toggle');
+    const spans = node.querySelectorAll("span.toc-toggle");
     for (const span of spans) {
         const css = span.classList;
-        css.remove('fa-caret-down');
-        css.add('fa-caret-right');
+        css.remove("fa-caret-down");
+        css.add("fa-caret-right");
     }
 }
 
 function expand(node) {
-    const root = document.getElementById('toc').querySelector('ul');
+    const root = document.getElementById("toc").querySelector("ul");
     // Show the current node and its ancestors.
     let parent = node;
     while (parent !== root) {
         // Show the node.
-        parent.classList.remove('hidden');
+        parent.classList.remove("hidden");
         // Show the expanded icon.
-        const span = parent.querySelector(':scope > span.toc-toggle');
+        const span = parent.querySelector(":scope > span.toc-toggle");
         if (span) {
             const css = span.classList;
-            css.remove('fa-caret-right');
-            css.add('fa-caret-down');
+            css.remove("fa-caret-right");
+            css.add("fa-caret-down");
         }
         parent = parent.parentNode;
     }
     // Show the children.
-    const children = node.querySelector(':scope > ul');
+    const children = node.querySelector(":scope > ul");
     if (children) {
-        children.classList.remove('hidden');
+        children.classList.remove("hidden");
     }
 }
 
 function toggle(span) {
     const css = span.classList;
-    const expanded = css.contains('fa-caret-down');
+    const expanded = css.contains("fa-caret-down");
     if (expanded) {
         collapse(span.parentNode);
     } else {
