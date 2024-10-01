1fe842c49ab497f5fc7517855a5c1842a10107fc
Simone Bordet <simone.bordet@gmail.com>
2024-10-01 22:11:49 +0200

    Fixes #1898 - Redirect download.cometd.org to Maven Central
    Removed upload of CometD distribution from release script.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- cometd-distribution/src/main/scripts/release.sh
+++ cometd-distribution/src/main/scripts/release.sh
@@ -17,9 +17,6 @@ echo "Running release script with arguments" "$@"
 COMETD_DIR=$1
 VERSION=$2
 
-echo "Uploading distribution"
-dd status=progress if=${COMETD_DIR}/cometd-distribution/target/cometd-${VERSION}-distribution.tar.gz | ssh ubuntu@download.cometd.org "sudo -u www-data dd of=/var/www/download.cometd.org/cometd-${VERSION}-distribution.tar.gz"
-
 echo "Uploading javadocs"
 cd ${COMETD_DIR}/cometd-java
 mvn javadoc:aggregate-jar
