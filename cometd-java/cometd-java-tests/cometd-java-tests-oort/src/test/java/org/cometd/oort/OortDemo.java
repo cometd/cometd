0f033c2e6fe07f8a854305869ba295162a89ff10
Simone Bordet <simone.bordet@gmail.com>
2025-08-15 17:52:11 +0200

    Fixes #2158 - Fix OortDemo to avoid hardcoded string class names and versions.
    Avoid hardcoded paths and versions.
    
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- cometd-java/cometd-java-tests/cometd-java-tests-oort/src/test/java/org/cometd/oort/OortDemo.java
+++ cometd-java/cometd-java-tests/cometd-java-tests-oort/src/test/java/org/cometd/oort/OortDemo.java
@@ -17,11 +17,16 @@
 package org.cometd.oort;
 
 import java.lang.management.ManagementFactory;
-import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
+import org.cometd.bayeux.server.BayeuxServer;
 import org.cometd.oort.jakarta.OortMulticastConfigServlet;
 import org.cometd.oort.jakarta.SetiServlet;
 import org.cometd.server.http.jakarta.CometDServlet;
+import org.eclipse.jetty.ee10.servlet.DefaultServlet;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.jmx.MBeanContainer;
@@ -32,8 +37,8 @@ import org.eclipse.jetty.util.resource.ResourceFactory;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 /**
- * Main class for cometd demo.
- * This is of use when running demo in a terracotta cluster
+ * Main class for the CometD Oort demo.
+ * This is of use when running in a cluster.
  */
 public class OortDemo {
     private final Server _server;
@@ -45,13 +50,11 @@ public class OortDemo {
     }
 
     public OortDemo(int port) throws Exception {
-        String base = ".";
-
         QueuedThreadPool qtp = new QueuedThreadPool();
         qtp.setMinThreads(5);
         qtp.setMaxThreads(200);
 
-        // Manually contruct context to avoid hassles with webapp classloaders for now.
+        // Manually construct context to avoid hassles with webapp classloaders for now.
         _server = new Server(qtp);
 
         // Setup JMX
@@ -67,40 +70,43 @@ public class OortDemo {
 
         ServletContextHandler context = new ServletContextHandler("/", ServletContextHandler.SESSIONS);
         contexts.addHandler(context);
-        context.addServlet("org.eclipse.jetty.servlet.DefaultServlet", "/");
+        context.addServlet(DefaultServlet.class, "/");
 
-        context.setBaseResource(ResourceFactory.of(_server).newResource(List.of(
-                URI.create(base + "/../../cometd-demo/src/main/webapp/"),
-                URI.create(base + "/../../cometd-demo/target/cometd-demo-2.4.0-SNAPSHOT/")
-        )));
+        try (Stream<Path> demoTargetPaths = Files.list(Paths.get("../../../cometd-demo/target"))) {
+            Path path = demoTargetPaths
+                    .filter(Files::isDirectory)
+                    .filter(p -> p.getFileName().toString().startsWith("cometd-demo-"))
+                    .findFirst()
+                    .orElseThrow();
+            context.setBaseResource(ResourceFactory.of(_server).newResource(List.of(path.toUri())));
+        }
 
-        ServletHolder cometd_holder = new ServletHolder(CometDServlet.class);
-        cometd_holder.setInitParameter("timeout", "200000");
-        cometd_holder.setInitParameter("interval", "100");
-        cometd_holder.setInitParameter("maxInterval", "100000");
-        cometd_holder.setInitParameter("multiSessionInterval", "1500");
-        cometd_holder.setInitOrder(1);
-        context.addServlet(cometd_holder, "/cometd/*");
+        ServletHolder cometdHolder = new ServletHolder(CometDServlet.class);
+        cometdHolder.setInitParameter("timeout", "20000");
+        cometdHolder.setInitParameter("allowedTransports", "long-polling");
+        cometdHolder.setInitOrder(1);
+        context.addServlet(cometdHolder, "/cometd/*");
 
-        ServletHolder oort_holder = new ServletHolder(OortMulticastConfigServlet.class);
-        oort_holder.setInitParameter(OortMulticastConfigServlet.OORT_URL_PARAM, "http://localhost:" + port + "/cometd");
-        oort_holder.setInitParameter(OortMulticastConfigServlet.OORT_CHANNELS_PARAM, "/chat/**");
-        oort_holder.setInitOrder(2);
-        context.addServlet(oort_holder, "/oort/*");
+        ServletHolder oortHolder = new ServletHolder(OortMulticastConfigServlet.class);
+        oortHolder.setInitParameter(OortMulticastConfigServlet.OORT_URL_PARAM, "http://localhost:" + port + "/cometd");
+        oortHolder.setInitParameter(OortMulticastConfigServlet.OORT_CHANNELS_PARAM, "/chat/**");
+        oortHolder.setInitOrder(2);
+        context.addServlet(oortHolder, "/oort/*");
 
-        ServletHolder seti_holder = new ServletHolder(SetiServlet.class);
-        seti_holder.setInitOrder(2);
-        context.addServlet(seti_holder, "/seti/*");
+        ServletHolder setiHolder = new ServletHolder(SetiServlet.class);
+        setiHolder.setInitOrder(2);
+        context.addServlet(setiHolder, "/seti/*");
 
         ServletHolder demo_holder = new ServletHolder(OortDemoServlet.class);
         demo_holder.setInitOrder(3);
         context.getServletHandler().addServlet(demo_holder);
 
-        context.setInitParameter("org.eclipse.jetty.server.context.ManagedAttributes", "org.cometd.bayeux,org.cometd.oort.Oort");
+        String names = String.join(",", BayeuxServer.ATTRIBUTE, Oort.OORT_ATTRIBUTE, Seti.SETI_ATTRIBUTE);
+        context.setInitParameter(ServletContextHandler.MANAGED_ATTRIBUTES, names);
 
         _server.start();
 
         Oort oort = (Oort)context.getServletContext().getAttribute(Oort.OORT_ATTRIBUTE);
-        assert (oort != null);
+        assert oort != null;
     }
 }
